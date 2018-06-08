7c07cccaa28145fed41c7b5d25ef12288e4e8f08
Marwolf <cleako@gmail.com>
2018-06-08 13:22:33 -0400

    Improved run

--- server/run.sh
+++ server/run.sh
@@ -1,1 +1,1 @@
-ant -f nbbuild.xml -Dnb.internal.action.name=run run
+java -jar "dist/Open_RSC_Server.jar" config/config.xml -Xmn512m -Xms1024m -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=2 -XX:NewRatio=8
