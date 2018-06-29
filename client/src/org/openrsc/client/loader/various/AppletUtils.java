c1b20e278798db31ba7b98818503f516398c508d
@Marwood_Wolf <cleako@gmail.com>
2018-06-28 20:00:57 -0400

    Removal of cache download

--- client/src/org/openrsc/client/loader/various/AppletUtils.java
+++ client/src/org/openrsc/client/loader/various/AppletUtils.java
@@ -22,9 +22,9 @@ public class AppletUtils {
 	public static int height = 334;
 
 	public static final File CACHE = new File(
-			System.getProperty("user.home") + File.separator + ".openrsc");
+			System.getProperty("user.home") + File.separator + "OpenRSC");
 
-	public static final File CACHEFILE = new File("./openrsc.zip");
+	public static final File CACHEFILE = new File("cache.zip");
 
 	public static String DISPLAY_MESSAGE = "Loading...";
 
