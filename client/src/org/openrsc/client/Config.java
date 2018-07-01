19ddcb5e245f0275fc62ffe5507e27999999097e
@Marwood_Wolf <cleako@gmail.com>
2018-07-01 16:29:04 -0400

    config cleanup

--- client/src/org/openrsc/client/Config.java
+++ client/src/org/openrsc/client/Config.java
@@ -4,16 +4,11 @@ import java.net.InetSocketAddress;
 
 public class Config 
 {
-	public static String IP = "game.openrsc.com";
-	public static int PORT = 53595;
-	public static InetSocketAddress ADDR = new InetSocketAddress(IP, PORT);	
-	public static int CLIENT_VERSION = 7;
-
-	public final static String TOO_MANY_CHARS_IN_WILDERNESS_TEXT_A = "Wilderness IP Limit Exceeded";
-	public final static String TOO_MANY_CHARS_IN_WILDERNESS_TEXT_B = "";
-    
-    public final static int DEFAULT_WINDOW_WIDTH = 512; // Original for OpenRSC: 800 ... Authentic RSC uses 512
-	public final static int DEFAULT_WINDOW_HEIGHT = 346; // Original for OpenRSC: 534 ... Authentic RSC uses 346
-    
-    public static final int NOTE_ITEM_ID_BASE = 10000;
+        public static String IP = "game.openrsc.com";
+        public static int PORT = 53595;
+        public static InetSocketAddress ADDR = new InetSocketAddress(IP, PORT);	
+        public static int CLIENT_VERSION = 7;  
+        public final static int DEFAULT_WINDOW_WIDTH = 512;
+        public final static int DEFAULT_WINDOW_HEIGHT = 346;    
+        public static final int NOTE_ITEM_ID_BASE = 10000;
 }
