b4e2cca0a7ba2069e27de10fdff08eed296ade29
Marwolf <cleako@gmail.com>
2018-07-02 10:09:05 -0400

    Set avatar images to be saved locally in avatars folder

--- server/src/org/openrsc/server/Config.java
+++ server/src/org/openrsc/server/Config.java
@@ -12,13 +12,13 @@ public class Config {
 	public static int RUNECRAFTING_AMOUNT_MULTIPLIER;
 	/** The maximum number of players allowed in the wilderness per IP */
 	public static int ALLOWED_CONCURRENT_IPS_IN_WILDERNESS = 2;
-    
+
     public static final int NOTE_ITEM_ID_BASE = 10000;
-	
+
 	public static final String WILDERNESS_ENTRY_BLOCKED_MESSAGE = "You may only enter the wilderness on " + Config.ALLOWED_CONCURRENT_IPS_IN_WILDERNESS + " character(s) at a time.";
-	
-	public static String AVATAR_DIR = "/usr/share/nginx/html/";
-	
+
+	public static String AVATAR_DIR = "avatars/";
+
 	public static void initConfig(File file) throws IOException {
 		SERVER_NAME    = "Open RSC";
 		PREFIX         = "@gre@OpenRSC:@whi@ "; // Prefix that is sent before every custom (non-RSC) message (such as commands).
@@ -37,33 +37,33 @@ public class Config {
 		{
 			AVATAR_DIR = dir;
 		}
-		
+
 		SERVER_VERSION = Integer.parseInt(props.getProperty("version"));
 		SERVER_IP      = props.getProperty("ip");
 		SERVER_PORT    = Integer.parseInt(props.getProperty("port"));
 		WEB_PORT       = Integer.parseInt(props.getProperty("webport"));
-		
+
 		DB_HOST  = props.getProperty("dbhost");
 		DB_NAME  = props.getProperty("dbname");
 		DB_LOGIN = props.getProperty("dblogin");
 		DB_PASS  = props.getProperty("dbpass");
-                
+
                 HMAC_PASS  = props.getProperty("hmac_pass");
-		
+
 		CONFIG_DB_NAME = props.getProperty("configdbname");
 		LOG_DB_NAME    = props.getProperty("logdbname");
 		TOOLS_DB_NAME = props.getProperty("toolsdbname");
-		
+
 		combat_xp = Float.parseFloat(props.getProperty("combat_xp"));
 		combat_xp_sub = Float.parseFloat(props.getProperty("combat_xp_sub"));
 		skill_xp = Float.parseFloat(props.getProperty("skill_xp"));
 		skill_xp_sub = Float.parseFloat(props.getProperty("skill_xp_sub"));
 		wild_xp_bonus = Float.parseFloat(props.getProperty("wild_xp_bonus"));
 		skulled_xp_bonus = Float.parseFloat(props.getProperty("skulled_xp_bonus"));
-		
-		
+
+
 		RUNECRAFTING_AMOUNT_MULTIPLIER    = Integer.parseInt(props.getProperty("runecrafting_amount_multiplier"));
-		
+
 		if(props.containsKey("staff_teleport_locations_db"))
 		{
 			STAFF_TELEPORT_LOCATION_DATABASE = props.getProperty("staff_teleport_locations_db");
@@ -76,9 +76,9 @@ public class Config {
 	public static float combat_xp, combat_xp_sub, skill_xp, skill_xp_sub, wild_xp_bonus, skulled_xp_bonus;
 	public static long START_TIME;
 	public static boolean LOGGING, IRC, IRC_DEBUG, IRC_NOTIFY, PK_MODE;
-	
+
 	public static boolean ALLOW_WEAKENS = true, ALLOW_GODSPELLS = true;
-    
+
 	public static final class Quests {
 		public static final int BLACK_KNIGHTS_FORTRESS = 0;
 		public static final int COOKS_ASSISTANT = 1;
@@ -130,10 +130,10 @@ public class Config {
 		public static final int DIGSITE = 47;
 		public static final int GERTRUDES_CAT = 48;
 		public static final int LEGENDS_QUEST = 49;
-        
+
         public static final int JOIN_BLACKARM_GANG = 50;
         public static final int JOIN_PHOENIX_GANG = 51;
-        
+
         public static final int TUTORIAL_ISLAND = 100;
 	}
 }
