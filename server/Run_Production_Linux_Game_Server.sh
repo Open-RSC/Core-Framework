60a88783ea13929b7c80787174de80a24d77e7b5
Marwolf <cleako@gmail.com>
2018-07-16 17:36:00 -0400

    Script cleanup

--- server/Run_Production_Linux_Game_Server.sh
+++ server/Run_Production_Linux_Game_Server.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-echo "Launching in a new window. Type 'screen -r' to view the live server screen."
-echo "Use CTRL + A + D to detach the live server screen."
+
+# Check for any updates to the game server
+echo "Pulling recent updates from the Open-RSC Game repository."
+echo ""
+echo ""
+sudo make pull
+echo ""
+echo ""
+
+# Compile the game server
+echo "Compiling the game server."
+echo ""
+echo ""
+sudo ant compile
+echo ""
+echo ""
+
+# Run the game server in a detached screen
+echo "Launching the game server in a new screen."
+echo ""
+echo "Type 'screen -r' to access the game server screen."
+echo "Use CTRL + A + D to detach the live server screen so it runs in the background."
+echo ""
 echo ""
 screen -dmS name ./ant_launcher.sh
--- server/ant_launcher.sh
+++ server/ant_launcher.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-echo "Pulling recent updates from the Open-RSC Game repo"
-sudo make pull
-echo ""
-echo ""
 
-#Compile
-echo "Compiling the game server"
+# Run the game server
+echo ""
+echo "Running the game server. Press CTRL + C to shut it down or"
+echo "CTRL + A + D to detached the screen so this runs in the background."
 echo ""
 echo ""
-sudo ant compile
-
-#Run game server
-echo "Launching the game server"
-echo ""
-echo ""
-ant build.xml runserver
+ant runserver
