834d56e8991180fcdf631c287de9085e5eece6cb
Fefect <ketsh11@hotmail.com>
2018-08-14 10:38:34 +0200

    Authentic skill behaviour + skill loop settings
    0: Vanilla
    1: Semi-batch (continue til harvested)
    2: Full-batch (continue till inventory is full/resource depleted)
    (Also includes a single quest.config removal)

--- server/src/org/openrsc/server/npchandler/Aggie/Aggie.java
+++ server/src/org/openrsc/server/npchandler/Aggie/Aggie.java
@@ -5,21 +5,15 @@ package org.openrsc.server.npchandler.Aggie;
 
 import org.openrsc.server.Config;
 import org.openrsc.server.event.SingleEvent;
-import org.openrsc.server.model.Npc;
-import org.openrsc.server.model.ChatMessage;
-import org.openrsc.server.model.MenuHandler;
-import org.openrsc.server.model.World;
+import org.openrsc.server.model.*;
 import org.openrsc.server.event.DelayedQuestChat;
 import org.openrsc.server.event.MiniEvent;
-import org.openrsc.server.model.InvItem;
-import org.openrsc.server.model.Player;
-import org.openrsc.server.model.Quest;
 import org.openrsc.server.npchandler.NpcHandler;
 public class Aggie implements NpcHandler {
 	public void handleNpc(final Npc npc, final Player owner) throws Exception {
 		npc.blockedBy(owner);
 		owner.setBusy(true);
-		Quest q = owner.getQuest(Config.Quests.PRINCE_ALI_RESCUE);
+		Quest q = owner.getQuest(Quests.PRINCE_ALI_RESCUE);
 		if(q != null) {
 			if(q.finished()) {
 				questNotStarted(npc, owner);
