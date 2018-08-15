1e373db7fe59767efe3b595c25fe97c13b80d90f
Marwolf <cleako@gmail.com>
2018-08-14 22:50:44 -0400

    Fixed server auto restart

---
+++ server/run_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Kills all java processes - needed for server auto restart process.
+pkill -f 'java -jar'
+echo ""
+echo "Launching the game server in a new screen."
+echo ""
+echo "Type 'screen -r' to access the game server screen."
+echo "Use CTRL + A + D to detach the live server screen so it runs in the background."
+echo ""
+echo ""
+screen -dmS name ./ant_launcher.sh
