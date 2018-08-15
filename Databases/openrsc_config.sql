093aa207418f115d58fb449f00851e800a48c27f
Christopher Snow <csnow@live.ca>
2018-08-15 19:52:29 -0400

    Comment out Lost City Teleport

--- Databases/openrsc_config.sql
+++ Databases/openrsc_config.sql
@@ -9070,7 +9070,7 @@ INSERT INTO `def_spell` (`name`, `description`, `level`, `type`, `rune_count`, `
 ('Saradomin strike', 'Summons the power of Saradomin', 60, 2, 3, 33, 4, 619, 2, 31, 2, 560, 40),
 ('Flames of Zamorak', 'Summons the power of Zamorak', 60, 2, 3, 33, 1, 619, 2, 31, 4, 560, 41),
 ('Charge earth Orb', 'Needs to be cast on an earth obelisk', 60, 5, 3, 34, 30, 611, 1, 46, 3, 560, 42),
-('Lost City teleport', 'Teleports you to the Lost City', 61, 0, 2, 42, 2, 31, 2, NULL, NULL, 488, 43),
+--('Lost City teleport', 'Teleports you to the Lost City', 61, 0, 2, 42, 2, 31, 2, NULL, NULL, 488, 43),
 ('Wind wave', 'A strength 13 missile attack', 62, 2, 2, 33, 5, 619, 1, NULL, NULL, 576, 44),
 ('Charge Fire Orb', 'Needs to be cast on a fire obelisk', 63, 5, 3, 611, 1, 46, 3, 31, 30, 584, 45),
 ('Water wave', 'A strength 14 missile attack', 65, 2, 3, 32, 7, 33, 5, 619, 1, 600, 46),
