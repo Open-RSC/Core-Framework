8ab7c4724669786c01abe05924918bb2a57e26f6
Tessalicious <telethatelethateletha@gmail.com>
2018-08-16 16:24:32 +0900

    Can't log out in wildness [Bug] #280

--- server/src/org/openrsc/server/model/Player.java
+++ server/src/org/openrsc/server/model/Player.java
@@ -2571,8 +2571,7 @@ public final class Player extends Mob implements Watcher, Comparable<Player>
 	
 	public boolean canLogout()
 	{
-		if(deathmatchEvent != null || isBusy() || inCombat() || System.currentTimeMillis() - getCombatTimer() < 10000 || ((getLocation().wildernessLevel() > 1 && System.currentTimeMillis() - lastWalk < 7000))) return false;
-		
+		if(deathmatchEvent != null || isBusy() || inCombat() || System.currentTimeMillis() - getCombatTimer() < 10000) return false;
 		return true;
 	}
 	
