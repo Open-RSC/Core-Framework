10e62ebd8bb10d12e53d1112f8f76327175bb2ca
Christopher Snow <csnow@live.ca>
2018-08-28 18:49:34 -0400

    Configurable fight mode selector toggle in options menu.

--- server/src/com/openrsc/server/Constants.java
+++ server/src/com/openrsc/server/Constants.java
@@ -146,6 +146,7 @@ public final class Constants {
 		public static boolean INVENTORY_COUNT_TOGGLE = false;
 		public static boolean ZOOM_VIEW_TOGGLE = false;
 		public static boolean MENU_COMBAT_STYLE_TOGGLE = false;
+		public static boolean FIGHTMODE_SELECTOR_TOGGLE = false;
 
 		/**
 		 * 
@@ -197,6 +198,7 @@ public final class Constants {
 			INVENTORY_COUNT_TOGGLE = Boolean.parseBoolean(props.getProperty("inventory_count_toggle"));
 			ZOOM_VIEW_TOGGLE = Boolean.parseBoolean(props.getProperty("zoom_view_toggle"));
 			MENU_COMBAT_STYLE_TOGGLE = Boolean.parseBoolean(props.getProperty("menu_combat_style_toggle"));
+			FIGHTMODE_SELECTOR_TOGGLE = Boolean.parseBoolean(props.getProperty("fightmode_selector_toggle"));
 
 			START_TIME = System.currentTimeMillis();
 		}
