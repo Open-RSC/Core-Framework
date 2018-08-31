598ce331492509db5fb43b802daa7b7e5af49dda
Marwolf <cleako@gmail.com>
2018-08-31 14:06:54 -0400

    Nav buttons updated

--- RSCL_Launcher/src/com/loader/openrsc/frame/AppFrame.java
+++ RSCL_Launcher/src/com/loader/openrsc/frame/AppFrame.java
@@ -47,10 +47,10 @@ public class AppFrame extends JFrame
         this.addLogo();
         this.addButtons();
         this.addNewsBox();
-        //(this.postedDate = new JLabel(XMLReader.getNews().getMessages().get(0).getSplitDate())).setBounds(131, 116, 128, 8);
-        //this.postedDate.setFont(Utils.getFont("runescape_uf.ttf", 1, 10.0f));
-        //this.postedDate.setHorizontalAlignment(0);
-        //this.bg.add(this.postedDate);
+        /*(this.postedDate = new JLabel(XMLReader.getNews().getMessages().get(0).getSplitDate())).setBounds(131, 116, 128, 8);
+        this.postedDate.setFont(Utils.getFont("runescape_uf.ttf", 1, 10.0f));
+        this.postedDate.setHorizontalAlignment(0);
+        this.bg.add(this.postedDate);*/
         this.addMouseListener(new PositionListener(this));
         this.addMouseMotionListener(new PositionListener(this));
         this.pack();
@@ -110,11 +110,11 @@ public class AppFrame extends JFrame
 	
     
     private void addButtons() {
-        this.bg.add(new LinkButton("Website" , new Rectangle(27, 480, 119, 40)));
-        this.bg.add(new LinkButton("Forums", new Rectangle(158, 480, 119, 40)));
-        this.bg.add(new LinkButton("Shop", new Rectangle(288, 480, 119, 40)));
-        this.bg.add(new LinkButton("Support", new Rectangle(418, 480, 119, 40)));
-        this.bg.add(new LinkButton("Register", new Rectangle(548, 480, 119, 40)));
+        this.bg.add(new LinkButton("News" , new Rectangle(27, 480, 119, 40)));
+        this.bg.add(new LinkButton("Bug Reports", new Rectangle(158, 480, 119, 40)));
+        this.bg.add(new LinkButton("Discord", new Rectangle(288, 480, 119, 40)));
+        this.bg.add(new LinkButton("GitHub", new Rectangle(418, 480, 119, 40)));
+        this.bg.add(new LinkButton("FAQ", new Rectangle(548, 480, 119, 40)));
         (this.launch = new LaunchButton()).setBounds(797, 481, 174, 69);
         this.bg.add(this.launch);
         this.bg.add(new ControlButton(2, 958, 8, 10, 11));
