5bbab00a2bff89a3cce45d735eca3280e00cdb70
Marwolf <cleako@gmail.com>
2018-09-01 22:56:44 -0400

    Removal of extra sprites cache reference, added more nicename strings

--- Launcher/src/com/loader/openrsc/Constants.java
+++ Launcher/src/com/loader/openrsc/Constants.java
@@ -8,17 +8,7 @@ public class Constants
     public static final int SERVER_PORT = 43594;
     public static final String frameTitle = "Open RSC";
     public static final String base_url = "http://localhost/";
-    
-    public static final String HOME_URL = "http://localhost/";
-    public static final String FORUM_URL = "http://wolfkingdom.net/forum.php";
-    public static final String SHOP_URL = "http://wolfkingdom.net/shop.php";
-    public static final String SUPPORT_URL = "http://www.wolfkingdom.net/viewforum.php?id=8";
-    public static final String REGISTER_URL = "http://www.wolfkingdom.net/register.php";
-    
-    //public static final String CONF_DIR = System.getProperty("user.home") + File.separator + "OpenRSC";
     public static final String CONF_DIR = "Cache";
-	
-	public static String CACHE_URL = "http://game.wolfkingdom.net:8080/downloads/cache/";
-        public static String CLIENT_URL = "http://localhost/downloads/Open_RSC_Client.jar";
-   
+    public static String CACHE_URL = "http://localhost/downloads/cache/";
+    public static String CLIENT_URL = "http://localhost/downloads/Open_RSC_Client.jar";
 }
