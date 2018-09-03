55acda581abb610f1d88ed1b11cee1a67691d27e
Christopher Snow <csnow@live.ca>
2018-09-02 20:52:13 -0400

    Player does not recieve items when finishing the tutorial, so remove them from the code.

--- server/src/com/openrsc/server/net/rsc/handlers/TutorialHandler.java
+++ server/src/com/openrsc/server/net/rsc/handlers/TutorialHandler.java
@@ -25,45 +25,6 @@ public class TutorialHandler implements PacketHandler {
 			if(player.getCache().hasKey("tutorial")) {
 				player.getCache().remove("tutorial");
 			}
-			if (!player.getInventory().hasItemId(70)) { // bronze long sword
-				player.getInventory().add(new Item(70, 1));
-			}
-			if (!player.getInventory().hasItemId(108)) { // bronze large
-				player.getInventory().add(new Item(108, 1));
-			}
-			if (!player.getInventory().hasItemId(117)) { // bronze chain
-				player.getInventory().add(new Item(117, 1));
-			}
-			if (!player.getInventory().hasItemId(206)) { // bronze legs
-				player.getInventory().add(new Item(206, 1));
-			}
-			if (!player.getInventory().hasItemId(4)) { // wooden shield
-				player.getInventory().add(new Item(4, 1));
-			}
-			if (!player.getInventory().hasItemId(376)) { // net
-				player.getInventory().add(new Item(376, 1));
-			}
-			if (!player.getInventory().hasItemId(156)) { // bronze pickaxe
-				player.getInventory().add(new Item(156, 1));
-			}
-			if (!player.getInventory().hasItemId(33)) { // air runes
-				player.getInventory().add(new Item(33, 12));
-			}
-			if (!player.getInventory().hasItemId(35)) { // mind runes
-				player.getInventory().add(new Item(35, 8));
-			}
-			if (!player.getInventory().hasItemId(32)) { // water runes
-				player.getInventory().add(new Item(32, 3));
-			}
-			if (!player.getInventory().hasItemId(34)) { // earth runes
-				player.getInventory().add(new Item(34, 2));
-			}
-			if (!player.getInventory().hasItemId(36)) { // body runes
-				player.getInventory().add(new Item(36, 1));
-			}
-			if (!player.getInventory().hasItemId(1263)) { // sleeping bag
-				player.getInventory().add(new Item(1263, 1));
-			}
 			player.teleport(122, 647, false);
 			player.message("Skipped tutorial, welcome to Lumbridge");
 			World.getWorld().sendWorldAnnouncement("New adventurer @gre@" + player.getUsername() + "@whi@ has arrived in lumbridge!");
