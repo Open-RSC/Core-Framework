14dae193bfeda6595f8089a2f7076a19ab0aa348
Marwolf <cleako@gmail.com>
2018-09-09 15:02:52 -0400

    Code progress

---
+++ scripts/hard-reset.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+NC=`tput sgr0` # No Color
+
+
+echo ""
+echo "${RED}Open RSC Hard Reset:${NC}
+An easy to use RSC private server framework.
+
+What do you wish to do?
+
+Choices:
+  ${RED}1${NC} - Perform a hard reset of everything
+  ${RED}2${NC} - Return to the main menu"
+echo ""
+echo "Which of the above do you wish to do? Type the choice number and press enter."
+echo ""
+read reset
+
+if [ "$reset" == "2" ]; then
+    make go
+else
+    sudo make stop
+    sudo rm -rf Website
+    sudo rm -rf data/db/mysql
+    sudo rm -rf etc/letsencrypt/live
+    sudo git reset HEAD --hard
+    sudo git pull
+fi
