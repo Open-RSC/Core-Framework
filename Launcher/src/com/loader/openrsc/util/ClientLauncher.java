33f1901738c2a85ed8209df392eb06de21280b24
Marwolf <cleako@gmail.com>
2018-09-15 00:48:37 -0400

    Launcher name set to "OpenRSC" for Discord game name status

--- Launcher/src/com/loader/openrsc/util/ClientLauncher.java
+++ Launcher/src/com/loader/openrsc/util/ClientLauncher.java
@@ -9,7 +9,7 @@ import java.net.URLClassLoader;
 import javax.swing.JFrame;
 
 import com.loader.openrsc.Constants;
-import com.loader.openrsc.Launcher;
+import com.loader.openrsc.OpenRSC;
 import com.loader.openrsc.frame.AppFrame;
 
 public class ClientLauncher
@@ -37,6 +37,7 @@ public class ClientLauncher
 		gameFrame.setMinimumSize(new Dimension(512 + 16, 334 + 49));
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameFrame.getContentPane().add(applet);
+                gameFrame.setAlwaysOnTop(false);
 		applet.init();
 		applet.start();
 		gameFrame.pack();
@@ -48,15 +49,15 @@ public class ClientLauncher
 			loader = new URLClassLoader(new URL[] { new URL(Constants.CLIENT_URL) });
 			mainClass = Class.forName("rsc.RSCFrame", true, loader);
 			if (loader == null) {
-				Launcher.getPopup().setMessage("Client failed to launch. Please try again or notify staff.");
-				Launcher.getPopup().showFrame();
+				OpenRSC.getPopup().setMessage("Client failed to launch. Please try again or notify staff.");
+				OpenRSC.getPopup().showFrame();
 				AppFrame.get().getLaunch().setEnabled(true);
 				return;
 			}
 		}
 		catch (Exception e) {
-			Launcher.getPopup().setMessage("Client failed to launch. Please try again or notify staff.");
-			Launcher.getPopup().showFrame();
+			OpenRSC.getPopup().setMessage("Client failed to launch. Please try again or notify staff.");
+			OpenRSC.getPopup().showFrame();
 			e.printStackTrace();
 		}
 	}
