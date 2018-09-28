604072b106b9122a04674b25438c0f148604b808
Marwolf <cleako@gmail.com>
2018-09-28 15:04:43 -0400

    Simplifies installation

--- Go-Windows.cmd
+++ Go-Windows.cmd
@@ -75,166 +75,18 @@ goto start
 :install
 echo:
 echo:
-echo Do you Chocolatey already installed? This is required to use the script.
-echo:
-echo Choices:
-echo   %RED%1%NC% - No, install for me!
-echo   %RED%2%NC% - I'm all set, continue!
-echo   %RED%3%NC% - Just install everything this needs, I don't care.
-echo:
-
-SET /P choco=Please enter a number choice from above:
-echo:
-if /i "%choco%"=="1" goto installchoco
-if /i "%choco%"=="2" goto askjava
-if /i "%choco%"=="3" goto installeverything
-
-echo Error! %choco% is not a valid option. Press enter to try again.
-echo:
-SET /P choco=""
-goto install
-
-
-:installchoco
+echo Installing everything needed. This will take a while, please do not close the window.
 echo:
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-echo:
-echo:
-echo Installing basic software needed to run the rest of this script.
-echo:
-choco install -y 7zip make
-echo:
-goto askjava
-:<------------End Install------------>
-
-
-
-:<------------Start Install Everything------------>
-:installeverything
-echo:
-echo:
-echo Installing everything. This will take a while, do not close the window.
-echo:
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install -y 7zip make jdk8 ant virtualbox docker-toolbox docker-compose gitkraken netbeans intellijidea-community
+choco install -y 7zip make jdk8 ant virtualbox docker-toolbox docker-compose
 explorer "C:\Users\Wolf\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Docker"
 echo:
 echo:
-echo Double click on "Docker Quickstart Terminal" and wait until it shows a whale.
+echo Please double click on "Docker Quickstart Terminal" and wait until it shows a whale.
 echo That will indicate when Docker Toolbox is configured and thus ready.
-SET /P install="Then come back here and press enter to continue."
-:<------------End Install Everything------------>
-
-
-:<------------Begin Java------------>
-:askjava
-echo:
-echo:
-echo Do you have Oracle Java JDK 8 and Apache Ant installed already? It is required for this.
-echo:
-echo Choices:
-echo   %RED%1%NC% - No, install for me!
-echo   %RED%2%NC% - I'm all set, continue!
-echo:
-
-SET /P java=Please enter a number choice from above:
-echo:
-if /i "%java%"=="1" goto installjava
-if /i "%java%"=="2" goto askdocker
-
-echo Error! %java% is not a valid option. Press enter to try again.
-echo:
-SET /P java=""
-goto askjava
-
-
-:installjava
-echo:
-echo:
-echo Installing Oracle Java JDK 8 and Apache Ant.
-echo:
-choco install -y jdk8 ant
-echo:
-goto askdocker
-:<------------End Java------------>
-
-
-:<------------Begin Docker------------>
-:askdocker
-echo:
-echo:
-echo Do you have Docker, Docker-Compose, and VirtualBox installed already? These are required.
-echo:
-echo Choices:
-echo   %RED%1%NC% - No, install for me!
-echo   %RED%2%NC% - I'm all set, continue!
-echo:
-
-SET /P docker=Please enter a number choice from above:
-echo:
-if /i "%docker%"=="1" goto installdocker
-if /i "%docker%"=="2" goto askgit
-
-echo Error! %docker% is not a valid option. Press enter to try again.
-echo:
-SET /P docker=""
-goto askdocker
-
-
-:installdocker
-echo:
-echo:
-echo Installing Docker, Docker Compose, and VirtualBox
-echo:
-echo choco install -y virtualbox docker-toolbox docker-compose
-echo:
-explorer "C:\Users\Wolf\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Docker"
-echo:
-echo:
-echo Double click on "Docker Quickstart Terminal" and wait until it shows a whale.
-echo That will indicate when Docker Toolbox is configured and thus ready.
-SET /P install="Then come back here and press enter to continue."
-echo:
-echo:
-echo Adding port forwarding rules to VirtualBox
-echo:
-make docker-toolbox-forward-windows
-echo:
-goto askgit
-:<------------End Docker------------>
-
-
-:<------------Begin Git------------>
-:askgit
-echo:
-echo:
-echo Do you have Git installed already? It is required for this.
-echo:
-echo Choices:
-echo   %RED%1%NC% - No, install for me!
-echo   %RED%2%NC% - I'm all set, continue!
-echo:
-SET /P git=Please enter a number choice from above:
-echo:
-if /i "%git%"=="1" goto installgit
-if /i "%git%"=="2" goto edition
-
-echo Error! %git% is not a valid option. Press enter to try again.
-echo:
-SET /P git=""
-goto askgit
-
-
-:installgit
-echo:
-echo:
-echo Installing Git.
-echo:
-choco install -y git
-echo:
-echo:
+SET /P install="Then come back here and press enter a few times to continue."
 goto edition
-:<------------End Git------------>
+:<------------End Install Everything------------>
 
 
 :<------------Begin Edition------------>
@@ -244,8 +96,8 @@ echo:
 echo Which edition of Open RSC would you like?
 echo:
 echo Choices:
-echo  %RED%1%NC% - Simple single player mode
-echo  %RED%2%NC% - Developer mode (includes everything)
+echo  %RED%1%NC% - Single player mode (only the bare minimum needed to play)
+echo  %RED%2%NC% - Developer mode (everything is included)
 echo:
 SET /P edition=Please enter a number choice from above:
 echo:
@@ -293,8 +145,8 @@ echo:
 echo Would you like to install Git Kraken? It simplifies git!
 echo:
 echo Choices:
-echo   %RED%1%NC% - Install for me!
-echo   %RED%2%NC% - I'm all set, continue!
+echo   %RED%1%NC% - Yes
+echo   %RED%2%NC% - I'm all set, lets continue!
 echo:
 SET /P gitkraken=Please enter a number choice from above:
 echo:
@@ -363,15 +215,6 @@ goto askide
 
 
 :developerstart
-
-echo Extracting client cache
-echo:
-echo md "%HOMEPATH%/OpenRSC"
-echo 7z x "Game/client/cache.zip" -o"%HOMEPATH%/OpenRSC"
-
-
-
-
 echo:
 echo:
 echo Starting Docker containers and downloading what is needed. This may take a while the first time.
@@ -382,8 +225,7 @@ echo:
 echo:
 echo Downloading a copy of the Website repository
 echo:
-make clone-windows-website
-make pull-website-windows
+make clone-website-windows
 echo:
 echo:
 echo Importing the game and forum databases.
@@ -400,9 +242,10 @@ goto start
 :update
 echo:
 echo:
-echo Checking for updates to the Docker-Home repository.
+echo Checking for updates
 echo:
 git pull
+make pull-website-windows:
 echo:
 goto start
 :<------------End Update------------>
