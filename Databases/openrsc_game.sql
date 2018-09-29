a7d4ad9cc5d14b3da67f9b6dde40653e3c2b5dc7
ipkpjersi <33754783+ipkpjersi@users.noreply.github.com>
2018-09-28 22:01:16 -0400

    Added npckills table

--- Databases/openrsc_game.sql
+++ Databases/openrsc_game.sql
@@ -27,6 +27,21 @@ USE `openrsc_game`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `openrsc_npckills`
+--
+
+DROP TABLE IF EXISTS `openrsc_npckills`;
+CREATE TABLE IF NOT EXISTS `openrsc_npckills` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `npcID` int(10) DEFAULT NULL,
+  `playerID` int(10) DEFAULT NULL,
+  `killCount` int(10) DEFAULT 0,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `openrsc_achievements`
 --
 
