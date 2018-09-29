6c91d5054efb4068486d741984c4cc97a059fd0a
Marwolf <cleako@gmail.com>
2018-09-29 12:24:55 -0400

    Minimum requirements updated, improved clone script

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -9,11 +9,8 @@ NC=`tput sgr0` # No Color
 clear
 
 echo ""
-echo "${RED}Open RSC:${NC}"
-echo "An easy to use RSC private server framework."
-echo ""
-echo ""
-echo "Please wait while the core files are being downloaded."
+echo "${RED}Open RSC${NC}"
+echo "An easy to use RSC private server framework"
 echo ""
 git clone https://github.com/Open-RSC/Game.git
 cd Game && sudo chmod -R 777 . && "./Go-Linux.sh"
