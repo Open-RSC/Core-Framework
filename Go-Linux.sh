d1cdcaf8a289e8a31fe4ebbd51265a2a88e2527c
Marwolf <cleako@gmail.com>
2018-09-29 12:19:35 -0400

    Correction to curl command, enhanced clone script

--- Go-Linux.sh
+++ Go-Linux.sh
@@ -16,7 +16,7 @@ email=`cat .email`
 #
 # Install everything with this command:
 #
-# curl -sSL https://raw.githubusercontent.com/Open-RSC/Game/master/scripts/clone.sh | bash
+# bash <(curl -s https://raw.githubusercontent.com/Open-RSC/Game/2.0.0/scripts/clone.sh)
 
 echo ""
 echo "${RED}Open RSC:${NC}
--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,7 +4,13 @@ exec 0</dev/tty
 # Open RSC: A replica RSC private server framework
 
 clear
-echo "Gathering files"
+
+echo ""
+echo "${RED}Open RSC:${NC}"
+echo "An easy to use RSC private server framework."
+echo ""
+echo ""
+echo "Please wait while the core files are being downloaded."
 echo ""
 git clone https://github.com/Open-RSC/Game.git
 cd Game && sudo chmod -R 777 . && "./Go-Linux.sh"
