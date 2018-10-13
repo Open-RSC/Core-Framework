6c84bd92fb6d83e0af59a6801f9060d1fa2dc27c
Christopher Snow <csnow@live.ca>
2018-10-13 13:11:52 -0400

    Verify Silk Trader, Border Guard. Fix small issues in Duke and Ned.

--- server/plugins/com/openrsc/server/plugins/npcs/lumbridge/DukeOfLumbridge.java
+++ server/plugins/com/openrsc/server/plugins/npcs/lumbridge/DukeOfLumbridge.java
@@ -5,8 +5,6 @@ import com.openrsc.server.model.entity.npc.Npc;
 import com.openrsc.server.model.entity.player.Player;
 import com.openrsc.server.plugins.listeners.action.TalkToNpcListener;
 import com.openrsc.server.plugins.listeners.executive.TalkToNpcExecutiveListener;
-import com.openrsc.server.plugins.menu.Menu;
-import com.openrsc.server.plugins.menu.Option;
 
 import static com.openrsc.server.plugins.Functions.*;
 
@@ -15,38 +13,43 @@ public final class DukeOfLumbridge implements TalkToNpcExecutiveListener,
 
 	@Override
 	public void onTalkToNpc(final Player p, final Npc n) {
-		Menu defaultMenu = new Menu();
 		npcTalk(p, n, "Greetings welcome to my castle");
-		if (p.getQuestStage(Quests.DRAGON_SLAYER) >= 2
-				|| p.getQuestStage(Quests.DRAGON_SLAYER) < 0) {
+		String[] menu = {
+			"Have you any quests for me?",
+			"Where can I find money?"
+		};
+		if (p.getQuestStage(Quests.DRAGON_SLAYER) >= 2 || p.getQuestStage(Quests.DRAGON_SLAYER) < 0) {
+			menu = new String[] { // Dragon Slayer
+				"I seek a shield that will protect me from dragon's breath",
+				"Have you any quests for me?",
+				"Where can I find money?"
+			};
+			int choice = showMenu(p, n, menu);
+			handleResponse(p, n, choice);
+		}
+		else {
+			int choice = showMenu(p, n, menu);
+			handleResponse(p, n, choice + 1);
+		}
+	}
+
+	public void handleResponse(Player p, Npc n, int option) {
+		if (option == 0) { // Dragon Slayer
 			if (!hasItem(p, 420, 1)) {
-				defaultMenu
-						.addOption(new Option(
-								"I seek a shield that will protect me from dragon's breath") {
-							public void action() {
-								npcTalk(p, n,
-										"A knight going on a dragon quest hmm?");
-								npcTalk(p, n, "A most worthy cause");
-								npcTalk(p, n, "Guard this well my friend");
-								message(p, "The duke hands you a shield");
-								addItem(p, 420, 1);
-							}
-						});
+				npcTalk(p, n, "A knight going on a dragon quest hmm?",
+						"A most worthy cause",
+						"Guard this well my friend"
+				);
+				message(p, "The duke hands you a shield");
+				addItem(p, 420, 1);
 			}
 		}
-		defaultMenu.addOptions(new Option("Have you any quests for me?") {
-			public void action() {
-				npcTalk(p, n, "All is well for me");
-			}
-		}, new Option("Where can I find money?") {
-			public void action() {
-				playerTalk(p, n, "Where can I find money?");
-				npcTalk(p, n,
-						"I've heard the blacksmiths are prosperous amoung the peasantry");
-				npcTalk(p, n, "Maybe you could try your hand at that");
-			}
-		});
-		defaultMenu.showMenu(p);
+		else if (option == 1)
+			npcTalk(p, n, "All is well for me");
+		else if (option == 2) {
+			npcTalk(p, n, "I've heard the blacksmiths are prosperous amoung the peasantry");
+			npcTalk(p, n, "Maybe you could try your hand at that");
+		}
 	}
 
 	@Override
