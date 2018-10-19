c44c4edfa2debcb9c83ddcdfd35ff32a9371f9ef
Marwolf <cleako@gmail.com>
2018-10-19 08:37:44 -0500

    Launcher corrections, added optional WoW experience formula

--- Launcher/src/com/loader/openrsc/Constants.java
+++ Launcher/src/com/loader/openrsc/Constants.java
@@ -1,12 +1,12 @@
 package com.loader.openrsc;
 
 public class Constants {
-		public static Double VERSION_NUMBER = 20181015.190000;
-    public static final String SERVER_DOMAIN = "dev1.openrsc.com"; // Only used for the server status display
+    public static Double VERSION_NUMBER = 20181015.190000;
+    public static final String SERVER_DOMAIN = "game.openrsc.com"; // Only used for the server status display
     public static final int SERVER_PORT = 43594;
     public static final String GAME_NAME = "Open RSC";
-    public static final String base_url = "https://dev.openrsc.com/"; // Cache and client jar download locations depend on this
-    //public static final String rss_url = "https://openrsc.com/blog/rss"; // RSS feed only
+    public static final String base_url = "https://openrsc.com/"; // Cache and client jar download locations depend on this
+    //public static final String rss_url = "https://openrsc.com/blog/rss"; // RSS feed
     public static final String CONF_DIR = "Cache";
     public static String CACHE_URL = base_url + "downloads/cache/";
     public static String CLIENT_URL = base_url + "downloads/Open_RSC_Client.jar";
