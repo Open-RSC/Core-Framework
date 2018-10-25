7e13f2cbbcfab069991d9e9a230fda28b7ebb694
Payton J <kyplerr@gmail.com>
2018-10-24 22:31:39 -0700

    [Digsite] Add Missing NPCs
    Please close Issue #924.
    
    The 2 workmen and 11 skeletons from the underground part of the Digsite quest were missing.

---
+++ scripts/db/Digsite Quest/AddMissingWorkmen.sql
@@ -0,0 +1,9 @@
+START TRANSACTION;
+
+INSERT INTO openrsc_npclocs(id, startX, minX, maxX, startY, minY, maxY)
+VALUES(738, 25, 22, 30, 3345, 3339, 3347);
+
+INSERT INTO openrsc_npclocs(id, startX, minX, maxX, startY, minY, maxY)
+VALUES(738, 25, 22, 30, 3346, 3339, 3347);
+
+COMMIT;
