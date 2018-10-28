e4c10e0ffd96a1d0a9898d5e7a5dd01d131b75c5
Marwolf <cleako@gmail.com>
2018-10-28 13:29:57 -0400

    Changed to docker-ce repo test for ubuntu cosmic support

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -24,9 +24,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo add-apt-repository \
       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) \
-      stable"
+      test"
     sudo apt update
-    sudo apt install docker docker-compose -y
+    sudo apt install docker-ce docker-compose -y
     # Adds instance user to docker group so it can execute commands.
     sudo usermod -a -G docker ubuntu
     # Permits instance user to execute Docker commands without sudo
