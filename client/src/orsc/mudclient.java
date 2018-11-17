208f2c3b39fd3b8d332d616d2c2069fde77c5b31
Marwolf <cleako@gmail.com>
2018-11-17 13:26:49 -0500

    Prevents noted items from being sold to shops and as having value

--- client/src/orsc/mudclient.java
+++ client/src/orsc/mudclient.java
@@ -2976,7 +2976,7 @@ public final class mudclient implements Runnable {
 							int sellCost = GenUtil.computeItemCost(EntityHandler.getItemDef(id).getBasePrice(),
 									this.shopItemPrice[this.shopSelectedItemIndex], this.shopSellPriceMod, -30910, false, 1,
 									count, this.shopPriceMultiplier);
-							if (EntityHandler.getItemDef(id).getNotedFormOf() >= 0) {
+							/*if (EntityHandler.getItemDef(id).getNotedFormOf() >= 0) {
 
 								int originalItemID = EntityHandler.getItemDef(id).getNotedFormOf();
 								int originalItemPrice = 0;
@@ -2997,7 +2997,7 @@ public final class mudclient implements Runnable {
 								sellCost = GenUtil.computeItemCost(EntityHandler.getItemDef(originalItemID).getBasePrice(),
 										originalItemPrice, this.shopSellPriceMod, -30910, false, 1, originalItemCount,
 										this.shopPriceMultiplier);
-							}
+							}*/
 							this.getSurface().drawString(
 									EntityHandler.getItemDef(id).getName() + ": sell for " + sellCost + "gp each", 2 + xr,
 									yr + 239, 0xFFFF00, 1);
