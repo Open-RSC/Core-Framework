bacdf9a180bfeba09ea0a21f702fed2f731865f9
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2018-11-18 18:28:31 -0600

    Fix gob diplomacy dialogue, Aggie and Wyson

--- server/plugins/com/openrsc/server/plugins/quests/free/GoblinDiplomacy.java
+++ server/plugins/com/openrsc/server/plugins/quests/free/GoblinDiplomacy.java
@@ -29,49 +29,51 @@ public class GoblinDiplomacy implements QuestInterface,TalkToNpcListener,
 		final Npc otherGoblin = n.getID() == 151 ? World.getWorld().getNpc(152,
 				314, 330, 441, 457) : World.getWorld().getNpc(151, 321, 445,
 				326, 449);
-		;
 		if (n.getID() == 151 || n.getID() == 152) {
 			if (p.getQuestStage(this) == 0) {
 				if (n.getID() == 151) {
-					npcTalk(p, n, "Green armour best");
-					npcTalk(p, otherGoblin, "No, no red every time");
-					npcTalk(p, n, "Go away human, we busy");
+					npcTalk(p, n, "green armour best");
+					npcTalk(p, otherGoblin, "No no Red every time");
+					npcTalk(p, n, "go away human, we busy");
 				} else {
 					npcTalk(p, n, "Red armour best");
-					npcTalk(p, otherGoblin, "No, no green every time");
-					npcTalk(p, n, "Go away human, we busy");
+					npcTalk(p, otherGoblin, "No no green every time");
+					npcTalk(p, n, "go away human, we busy");
 				}
 			} else if (p.getQuestStage(this) == 1) {
 				if (n.getID() == 151) {
-					npcTalk(p, n, "Green armour best");
-					npcTalk(p, otherGoblin, "No, no red every time");
-					npcTalk(p, n, "Go away human, we busy");
+					npcTalk(p, n, "green armour best");
+					npcTalk(p, otherGoblin, "No no Red every time");
+					npcTalk(p, n, "go away human, we busy");
 				} else {
 					npcTalk(p, n, "Red armour best");
-					npcTalk(p, otherGoblin, "No, no green every time");
-					npcTalk(p, n, "Go away human, we busy");
+					npcTalk(p, otherGoblin, "No no green every time");
+					npcTalk(p, n, "go away human, we busy");
 				}
 
-				int option = showMenu(p, n,
+				int option = showMenu(p, n, false, //do not send over
 						"Why are you arguing about the colour of your armour?",
 						"Wouldn't you prefer peace?",
-						"Do you want me to pick an armour colour for you");
+						"Do you want me to pick an armour colour for you?");
 				switch (option) {
 				case 0: // yes
-					npcTalk(p, n, "We decide to celevrate goblin new century",
+					playerTalk(p, n, "Why are you arguing about the colour of your armour?");
+					npcTalk(p, n, "We decide to celebrate goblin new century",
 							"By changing the colour of our armour",
 							"Light blue get boring after a bit",
-							"And we cant change",
-							"Problem is we want different change to us");
+							"And we want change",
+							"Problem is they want different change to us");
 					break;
 				case 1: // No
+					playerTalk(p, n, "Wouldn't you prefer peace");
 					npcTalk(p, n,
-							"Yeah peace is good as long as it is peace wearing green armour");
+							"Yeah peace is good as long as it is peace wearing Green armour");
 					npcTalk(p, otherGoblin, "But green to much like skin!",
 							"Nearly make you look naked!");
 					break;
 				case 2:
-					playerTalk(p, n, "Different to either green or red");
+					playerTalk(p, n, "Do you want me to pick an armour colour for you?",
+							"different to either green or red");
 					npcTalk(p, n, "Hmm me dunno what that'd look like",
 							"You'd have to bring me some, so us could decide");
 					npcTalk(p, otherGoblin, "Yep bring us orange armour");
@@ -79,60 +81,53 @@ public class GoblinDiplomacy implements QuestInterface,TalkToNpcListener,
 					p.updateQuestStage(getQuestId(), 2);
 				}
 			} else if (p.getQuestStage(this) == 2) {
-				npcTalk(p, n, "Oh it you",
-						"Have you got some orange goblin armour yet?");
+				npcTalk(p, n, "Oh it you");
 				if (p.getInventory().hasItemId(274)) {
-					playerTalk(p, n, "Yeah I have it right here");
-					message(p, "You hand Wartface the armour");
+					playerTalk(p, n, "I have some orange armour");
+					message(p, "You give some goblin armour to the goblins");
 					p.getInventory().remove(274, 1);
 					npcTalk(p, n, "No I don't like that much");
 					npcTalk(p, otherGoblin, "It clashes with my skin colour");
 					npcTalk(p, n, "Try bringing us dark blue armour");
 					p.updateQuestStage(getQuestId(), 3);
 				} else {
+					npcTalk(p, n, "Have you got some orange goblin armour yet?");
 					playerTalk(p, n, "Err no");
 					npcTalk(p, n, "Come back when you have some");
 				}
 			} else if (p.getQuestStage(this) == 3) {
-				npcTalk(p, n, "Oh it you",
-						"Have you got us some dark blue goblin armour?");
+				npcTalk(p, n, "Oh it you");
 				if (p.getInventory().hasItemId(275)) {
-					playerTalk(p, n, "Yes, here you go");
-					p.message(
-							"You hand Wartface the armour");
+					playerTalk(p, n, "I have some dark blue armour");
+					message(p, "You give some goblin armour to the goblins");
 					p.getInventory().remove(275, 1);
 					npcTalk(p, n, "Doesn't seem quite right");
-					npcTalk(p, otherGoblin, "Maybe if it was a bit lighter");
+					npcTalk(p, otherGoblin, "maybe if it was a bit lighter");
 					npcTalk(p, n, "Yeah try light blue");
-					playerTalk(
-							p,
-							n,
-							"I thought that was the armour you were changing from",
-							"But never mind, anything is worth a try to avoid a war");
+					playerTalk(p, n,
+							"I thought that was the amour you were changing from",
+							"But never mind, anything is worth a try");
 					p.updateQuestStage(getQuestId(), 4);
 				} else {
-					npcTalk(p, n, "Not yet");
+					npcTalk(p, n, "Have you got some Dark Blue goblin armour yet?");
+					playerTalk(p, n, "Err no");
 					npcTalk(p, n, "Come back when you have some");
 				}
 			} else if (p.getQuestStage(this) == 4) {
-				npcTalk(p, n, "Oh it you",
-						"Have you got some light blue goblin armour yet?");
 				if (p.getInventory().hasItemId(273)) {
-					playerTalk(p, n, "Sigh...", "Yes, here it is");
-
-					p.message(
-							"You hand Wartface the armour");
+					playerTalk(p, n, "Ok I've got light blue armour");
+					message(p, "You give some goblin armour to the goblins");
 					p.getInventory().remove(273, 1);
-
 					npcTalk(p, n, "That is rather nice");
 					npcTalk(p, otherGoblin,
-							"Yes i could see myself wearing somethin' like that");
-					npcTalk(p, n, "It a deal then", "Light blue it is",
+							"Yes I could see myself wearing somethin' like that");
+					npcTalk(p, n, "It' a deal then", "Light blue it is",
 							"Thank you for sorting our argument");
 
 					p.sendQuestComplete(Constants.Quests.GOBLIN_DIPLOMACY);
 				} else {
-					npcTalk(p, n, "Not yet");
+					npcTalk(p, n, "Have you got some Light Blue goblin armour yet?");
+					playerTalk(p, n, "Err no");
 					npcTalk(p, n, "Come back when you have some");
 				}
 			} else if (p.getQuestStage(this) == -1) { // COMPLETED
@@ -153,9 +148,10 @@ public class GoblinDiplomacy implements QuestInterface,TalkToNpcListener,
 
 	@Override
 	public void handleReward(Player player) {
+		player.message("Well done you have completed the goblin diplomacy quest");
 		player.message("@gre@You haved gained 5 quest points!");
 		incQuestReward(player, Quests.questData.get(Quests.GOBLIN_DIPLOMACY), true);
-		player.message("General Wartface gives you a gold bar as thanks");
+		player.message("general wartface gives you a gold bar as thanks");
 		player.getInventory().add(new Item(172, 1));
 	}
 
