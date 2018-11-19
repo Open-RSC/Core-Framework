c94bd31dcc826bdb1a727ddfb0f443ca645ba493
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2018-11-19 12:26:07 -0600

    Fix up verbatim pirate treasure dialogue and customs officer

--- server/plugins/com/openrsc/server/plugins/npcs/karamja/BoatFromKaramja.java
+++ server/plugins/com/openrsc/server/plugins/npcs/karamja/BoatFromKaramja.java
@@ -21,15 +21,14 @@ public final class BoatFromKaramja implements TalkToNpcExecutiveListener,
 		if (option == 0) {
 			npcTalk(p, n, "You need to be searched before you can board");
 			int sub_opt = showMenu(p, n, "Why?",
-					"You're not putting your hands on my things!",
-					"Search away I have nothing to hide");
+					"Search away I have nothing to hide",
+					"You're not putting your hands on my things");
 			if (sub_opt == 0) {
 				npcTalk(p, n,
-						"Because asgarnia has banned the import of intoxicating spirits");
+						"Because Asgarnia has banned the import of intoxicating spirits");
 			} else if (sub_opt == 1) {
-				npcTalk(p, n, "You're not getting on this ship then");
-			} else if (sub_opt == 2) {
 				if (hasItem(p, 318, 1)) {
+					npcTalk(p, n, "Aha trying to smuggle rum are we?");
 					message(p, "The customs officer confiscates your rum");
 					removeItem(p, 318, 1);
 				} else {
@@ -37,18 +36,23 @@ public final class BoatFromKaramja implements TalkToNpcExecutiveListener,
 							n,
 							"Well you've got some odd stuff, but it's all legal",
 							"Now you need to pay a boarding charge of 30 gold");
-					if (showMenu(p, n, "Ok", "Oh, I'll not bother then") == 0) {
+					int pay_opt = showMenu(p, n, false, "Ok", "Oh, I'll not bother then");
+					if (pay_opt == 0) {
 						if (removeItem(p, 10, 30)) {
 							message(p, "You pay 30 gold", "You board the ship");
 							movePlayer(p, 269, 648, true);
-							p.message("You arrive at Port Sarim");
+							p.message("The ship arrives at Port Sarim");
 						} else { // not enough money
 							playerTalk(p, n,
-									"Oh dear it seems i don't have enough money");
+									"Oh dear I don't seem to have enough money");
 						}
+					} else if (pay_opt == 1) {
+						playerTalk(p, n, "Oh, I'll not bother then");
 					}
 				}
-			}
+			} else if (sub_opt == 2) {
+				npcTalk(p, n, "You're not getting on this ship then");
+			} 
 		} else if (option == 1) {
 			npcTalk(p, n, "I'm not that sort of customs officer");
 		}
