f08b7c68bb8020b56c05daca7514f28036459140
James Wallen-Jones <j.wallen.jones@googlemail.com>
2018-11-28 08:55:40 +0000

    Finish tidying up misc folder

--- server/plugins/com/openrsc/server/plugins/misc/TeleportStone.java
+++ server/plugins/com/openrsc/server/plugins/misc/TeleportStone.java
@@ -9,24 +9,22 @@ import static com.openrsc.server.plugins.Functions.*;
 
 public class TeleportStone implements InvActionListener, InvActionExecutiveListener {
 
+	// Pretty sure this item doesn't even exist.
 	private final int TELEPORT_STONE = 2107;
 
 	@Override
 	public boolean blockInvAction(Item item, Player player) {
-		if(item.getID() == TELEPORT_STONE) {
-			return true;
-		}
-		return false;
+		return item.getID() == TELEPORT_STONE;
 	}
 
 	@Override
 	public void onInvAction(Item item, Player p) {
-		if(item.getID() == TELEPORT_STONE) {
+		if (item.getID() == TELEPORT_STONE) {
 			message(p, "the stone starts shaking...");
 			p.message("a magical portal opens up, where would you like to go?");
-			String[] teleLoc = { "Lumbridge", "Draynor", "Falador", "Edgeville", "Varrock", "Alkharid", "Karamja", "Yanille", "Ardougne", "Catherby", "Seers", "Gnome Stronghold", "Stay here" };
+			String[] teleLoc = {"Lumbridge", "Draynor", "Falador", "Edgeville", "Varrock", "Alkharid", "Karamja", "Yanille", "Ardougne", "Catherby", "Seers", "Gnome Stronghold", "Stay here"};
 			int menu = showMenu(p, teleLoc);
-			if(p.getLocation().inWilderness() && System.currentTimeMillis() - p.getCombatTimer() < 10000) {
+			if (p.getLocation().inWilderness() && System.currentTimeMillis() - p.getCombatTimer() < 10000) {
 				p.message("You need to stay out of combat for 10 seconds before using a teleport.");
 				return;
 			}
@@ -35,59 +33,59 @@ public class TeleportStone implements InvActionListener, InvActionExecutiveListe
 				p.message("You can't use this teleport after level 30 wilderness");
 				return;
 			}
-			if(p.getInventory().countId(1039) > 0) {
-				message(p, "You can't teleport while holding Ana,", 
-						"It's just too difficult to concentrate.");
+			if (p.getInventory().countId(1039) > 0) {
+				message(p, "You can't teleport while holding Ana,",
+					"It's just too difficult to concentrate.");
 				return;
 			}
-			if(p.getInventory().hasItemId(812)) {
+			if (p.getInventory().hasItemId(812)) {
 				p.message("the plague sample is too delicate...");
 				p.message("it disintegrates in the crossing");
-				while(p.getInventory().countId(812) > 0) {
+				while (p.getInventory().countId(812) > 0) {
 					p.getInventory().remove(new Item(812));
 				}
 			}
-			switch(menu) {
-			case -1:// stop them.
-				return;
-			case 0: // lumb
-				p.teleport(125, 648);
-				break;
-			case 1: // dray
-				p.teleport(214, 632);
-				break;
-			case 2: // falla
-				p.teleport(304, 542);
-				break;
-			case 3: // edge
-				p.teleport(223, 447);
-				break;
-			case 4: // varrock
-				p.teleport(122, 509);
-				break;
-			case 5: // alkharid
-				p.teleport(85, 691);
-				break;
-			case 6: // Karamja
-				p.teleport(372, 706);
-				break;
-			case 7: // Yanille
-				p.teleport(583, 747);
-				break;
-			case 8: // Ardougne
-				p.teleport(557, 606);
-				break;
-			case 9: // Catherby
-				p.teleport(442, 503);
-				break;
-			case 10: // Seers
-				p.teleport(493, 456);
-				break;
-			case 11: // Gnome Stronghold
-				p.teleport(703, 481);
-				break;
-			case 12:
-				return;
+			switch (menu) {
+				case -1:// stop them.
+					return;
+				case 0: // lumb
+					p.teleport(125, 648);
+					break;
+				case 1: // dray
+					p.teleport(214, 632);
+					break;
+				case 2: // falla
+					p.teleport(304, 542);
+					break;
+				case 3: // edge
+					p.teleport(223, 447);
+					break;
+				case 4: // varrock
+					p.teleport(122, 509);
+					break;
+				case 5: // alkharid
+					p.teleport(85, 691);
+					break;
+				case 6: // Karamja
+					p.teleport(372, 706);
+					break;
+				case 7: // Yanille
+					p.teleport(583, 747);
+					break;
+				case 8: // Ardougne
+					p.teleport(557, 606);
+					break;
+				case 9: // Catherby
+					p.teleport(442, 503);
+					break;
+				case 10: // Seers
+					p.teleport(493, 456);
+					break;
+				case 11: // Gnome Stronghold
+					p.teleport(703, 481);
+					break;
+				case 12:
+					return;
 			}
 			removeItem(p, TELEPORT_STONE, 1);
 			sleep(650);
