241b71653d79afc1ad1657180b6dac54e18852c4
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2018-12-06 20:24:36 -0600

    Fix on correct poison for npcs that poison

--- server/src/com/openrsc/server/Constants.java
+++ server/src/com/openrsc/server/Constants.java
@@ -300,6 +300,23 @@ public final class Constants {
 				VIEW_DISTANCE = 4;
 		}
 	}
+	
+	public static final class Poison {
+		public static final int POISON_SCORPION = 271;
+		public static final int POISON_SPIDER = 292;
+		public static final int DUNGEON_SPIDER = 656;
+		public static final int TRIBESMAN = 421;
+		public static final int JUNGLE_SAVAGE = 776;
+		
+		public static final HashMap<Integer, Integer> npcData = new HashMap<Integer, Integer>()
+		{{
+			put(POISON_SCORPION, 38);
+			put(POISON_SPIDER, 68);
+			put(DUNGEON_SPIDER, 38);
+			put(TRIBESMAN, 68);
+			put(JUNGLE_SAVAGE, 68);
+		}};
+	}
 
 	public static final class Quests {
 		public static final int BLACK_KNIGHTS_FORTRESS = 0;
