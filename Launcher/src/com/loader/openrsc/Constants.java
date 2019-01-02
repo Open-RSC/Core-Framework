13f06ba328acce43f83efb707cac6da34c70ad89
Marwolf <cleako@gmail.com>
2019-01-02 17:33:16 -0500

    Launcher: optimized imports, rearranged code entries

--- Launcher/src/com/loader/openrsc/Constants.java
+++ Launcher/src/com/loader/openrsc/Constants.java
@@ -2,20 +2,20 @@ package com.loader.openrsc;
 
 public class Constants {
 
-    // Basic information
-    public static final String GAME_NAME = "Open RSC";
-    public static final String SERVER_DOMAIN = "game.openrsc.com"; // Only used for the server status display
-    public static final int SERVER_PORT = 43594;
+	// Basic information
+	public static final String GAME_NAME = "Open RSC";
+	public static final String SERVER_DOMAIN = "game.openrsc.com"; // Only used for the server status display
+	public static final int SERVER_PORT = 43594;
 
-    // Cache
-    public static final String BASE_URL = "https://game.openrsc.com/"; // Cache and client jar download locations depend on this
-    public static final String CONF_DIR = "Cache";
-    public static final String CLIENT_FILENAME = "Open_RSC_Client.jar";
-    public static final String CACHE_URL = BASE_URL + "downloads/cache/";
+	// Cache
+	public static final String BASE_URL = "https://game.openrsc.com/"; // Cache and client jar download locations depend on this
+	public static final String CONF_DIR = "Cache";
+	public static final String CLIENT_FILENAME = "Open_RSC_Client.jar";
+	public static final String CACHE_URL = BASE_URL + "downloads/cache/";
 
-    // Launcher version checking
-    public static final Double VERSION_NUMBER = 20181109.230000; //YYYYMMDD.HHMMSS format
-    public static final String VERSION_UPDATE_URL = "https://raw.githubusercontent.com/open-rsc/Game/2.0.0/Launcher/src/com/loader/openrsc/Constants.java";
-    public static final String UPDATE_JAR_URL = "https://game.openrsc.com/downloads/OpenRSC.jar";
-    public static final String JAR_FILENAME = "OpenRSC.jar";
+	// Launcher version checking
+	public static final Double VERSION_NUMBER = 20190102.170000; //YYYYMMDD.HHMMSS format
+	public static final String VERSION_UPDATE_URL = "https://raw.githubusercontent.com/open-rsc/Game/2.0.0/Launcher/src/com/loader/openrsc/Constants.java";
+	public static final String UPDATE_JAR_URL = "https://game.openrsc.com/downloads/OpenRSC.jar";
+	public static final String JAR_FILENAME = "OpenRSC.jar";
 }
--- Launcher/src/com/loader/openrsc/frame/elements/ControlButton.java
+++ Launcher/src/com/loader/openrsc/frame/elements/ControlButton.java
@@ -7,19 +7,19 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class ControlButton extends JButton {
-    public ControlButton(final int buttonType, final int x, final int y, final int width, final int height) {
-        final String image = (buttonType == 1) ? "minimize" : "close";
-        if (buttonType != 3) {
-            this.setIcon(Utils.getImage(String.valueOf(image) + ".png"));
-            this.setRolloverIcon(Utils.getImage(String.valueOf(image) + "_hover.png"));
-        }
-        this.setBorderPainted(false);
-        this.setContentAreaFilled(false);
-        this.setHorizontalTextPosition(0);
-        this.setActionCommand(image);
-        if (buttonType != 3) {
-            this.addActionListener(new ButtonListener());
-        }
-        this.setBounds(x, y, width, height);
-    }
+	public ControlButton(final int buttonType, final int x, final int y, final int width, final int height) {
+		final String image = (buttonType == 1) ? "minimize" : "close";
+		if (buttonType != 3) {
+			this.setIcon(Utils.getImage(String.valueOf(image) + ".png"));
+			this.setRolloverIcon(Utils.getImage(String.valueOf(image) + "_hover.png"));
+		}
+		this.setBorderPainted(false);
+		this.setContentAreaFilled(false);
+		this.setHorizontalTextPosition(0);
+		this.setActionCommand(image);
+		if (buttonType != 3) {
+			this.addActionListener(new ButtonListener());
+		}
+		this.setBounds(x, y, width, height);
+	}
 }
