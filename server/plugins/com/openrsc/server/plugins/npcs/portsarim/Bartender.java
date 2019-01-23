dfbe06e597264039bdd0f75a90e77fed8c2aace5
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2019-01-23 23:07:00 +0400

    Fix mechanics barcrawl miniquest and dialogues

--- server/plugins/com/openrsc/server/plugins/npcs/portsarim/Bartender.java
+++ server/plugins/com/openrsc/server/plugins/npcs/portsarim/Bartender.java
@@ -1,4 +1,4 @@
-package com.openrsc.server.plugins.npcs.falador;
+package com.openrsc.server.plugins.npcs.portsarim;
 
 import com.openrsc.server.Constants.Quests;
 import com.openrsc.server.model.entity.npc.Npc;
@@ -13,13 +13,21 @@ import static com.openrsc.server.plugins.Functions.*;
 public class Bartender implements TalkToNpcExecutiveListener, TalkToNpcListener {
 
 	@Override
+	public boolean blockTalkToNpc(Player p, Npc n) {
+		return n.getID() == 150;
+	}
+	
+	@Override
 	public void onTalkToNpc(final Player p, final Npc n) {
 		Menu defaultMenu = new Menu();
 		defaultMenu.addOption(new Option("Could i buy a beer please?") {
 			@Override
 			public void action() {
 				npcTalk(p, n, "Sure that will be 2 gold coins please");
-				if (p.getInventory().remove(10, 2) > -1) {
+				if (hasItem(p, 10, 2)) {
+					playerTalk(p, n, "Ok here you go thanks");
+					p.getInventory().remove(10, 2);
+					p.message("you buy a pint of beer");
 					addItem(p, 193, 1);
 				} else {
 					p.message("You dont have enough coins for the beer");
@@ -34,14 +42,13 @@ public class Bartender implements TalkToNpcExecutiveListener, TalkToNpcListener 
 					npcTalk(p,
 						n,
 						"No it was earlier",
-						"There was a guy in here saying the goblins up by the mountain",
-						"Are arguing again",
+						"There was a guy in here saying the goblins up by the mountain are arguing again",
 						"Of all things about the colour of their armour",
-						"Knowing the goblins,it could easily turn into a full blown war",
+						"Knowing the goblins, it could easily turn into a full blown war",
 						"Which wouldn't be good",
 						"Goblin wars make such a mess of the countryside");
 					playerTalk(p, n,
-						"Well if I have time I'll see if i can go and knock some sense into them");
+						"Well if I have time I'll see if I can go and knock some sense into them");
 					p.updateQuestStage(Quests.GOBLIN_DIPLOMACY, 1); // remember
 					// quest
 					// starts
@@ -69,6 +76,7 @@ public class Bartender implements TalkToNpcExecutiveListener, TalkToNpcListener 
 					npcTalk(p, n,
 						"Ok one black skull ale coming up, 8 coins please");
 					if (hasItem(p, 10, 8)) {
+						p.getInventory().remove(10, 8);
 						message(p, "You buy a black skull ale",
 							"You drink your black skull ale",
 							"Your vision blurs",
@@ -76,17 +84,12 @@ public class Bartender implements TalkToNpcExecutiveListener, TalkToNpcListener 
 						p.getCache().store("barsix", true);
 						playerTalk(p, n, "hiccup", "hiccup");
 					} else {
-						playerTalk(p, n, "I don't have 8 coins right now");
+						playerTalk(p, n, "I don't have 8 coins with me");
 					}
 				}
 			});
 		}
 		defaultMenu.showMenu(p);
-	}
-
-	@Override
-	public boolean blockTalkToNpc(Player p, Npc n) {
-		return n.getID() == 150;
 	}
 
 }
