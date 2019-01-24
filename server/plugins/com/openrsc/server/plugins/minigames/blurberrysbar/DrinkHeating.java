e1157a10bc79ed55dc39b9a083deee375c6c1f12
Christopher Snow <csnow@live.ca>
2019-01-24 17:28:59 -0500

    DrinkHeating.java: Add ItemId enum data over bare ints.

--- server/plugins/com/openrsc/server/plugins/minigames/blurberrysbar/DrinkHeating.java
+++ server/plugins/com/openrsc/server/plugins/minigames/blurberrysbar/DrinkHeating.java
@@ -1,5 +1,6 @@
 package com.openrsc.server.plugins.minigames.blurberrysbar;
 
+import com.openrsc.server.external.ItemId;
 import com.openrsc.server.model.container.Item;
 import com.openrsc.server.model.entity.GameObject;
 import com.openrsc.server.model.entity.player.Player;
@@ -13,10 +14,11 @@ public class DrinkHeating implements InvUseOnObjectListener, InvUseOnObjectExecu
 
 	@Override
 	public boolean blockInvUseOnObject(GameObject obj, Item item, Player p) {
-		if (item.getID() == 854 && obj.getID() == 119) {
+		if (item.getID() == ItemId.FULL_COCKTAIL_GLASS.id() && obj.getID() == 119) {
 			return true;
 		}
-		if ((item.getID() == 853 || item.getID() == 867) && obj.getID() == 119) {
+		if ((item.getID() == ItemId.HALF_COCKTAIL_GLASS.id() || item.getID() == ItemId.ODD_LOOKING_COCKTAIL.id())
+			&& obj.getID() == 119) {
 			return true;
 		}
 		return false;
@@ -24,11 +26,11 @@ public class DrinkHeating implements InvUseOnObjectListener, InvUseOnObjectExecu
 
 	@Override
 	public void onInvUseOnObject(GameObject obj, Item item, Player p) {
-		if (item.getID() == 854 && obj.getID() == 119) {
+		if (item.getID() == ItemId.FULL_COCKTAIL_GLASS.id() && obj.getID() == 119) {
 			message(p, "you briefly place the drink in the oven");
 			p.message("you remove the warm drink");
 			if (p.getCache().hasKey("drunk_dragon_base") && p.getCache().hasKey("diced_pa_to_drink") && p.getCache().hasKey("cream_into_drink")) {
-				p.getInventory().replace(854, 872);
+				p.getInventory().replace(ItemId.FULL_COCKTAIL_GLASS.id(), ItemId.DRUNK_DRAGON.id());
 				checkAndRemoveBlurberry(p, true);
 			}
 			if (p.getCache().hasKey("chocolate_saturday_base") && p.getCache().hasKey("choco_bar_in_drink")) {
@@ -36,10 +38,11 @@ public class DrinkHeating implements InvUseOnObjectListener, InvUseOnObjectExecu
 					p.getCache().store("heated_choco_saturday", true);
 				}
 			} else {
-				p.getInventory().replace(854, 867);
+				p.getInventory().replace(ItemId.FULL_COCKTAIL_GLASS.id(), ItemId.ODD_LOOKING_COCKTAIL.id());
 			}
 		}
-		if ((item.getID() == 853 || item.getID() == 867) && obj.getID() == 119) {
+		if ((item.getID() == ItemId.HALF_COCKTAIL_GLASS.id() || item.getID() == ItemId.ODD_LOOKING_COCKTAIL.id())
+			&& obj.getID() == 119) {
 			message(p, "you briefly place the drink in the oven");
 			p.message("you remove the warm drink");
 		}
