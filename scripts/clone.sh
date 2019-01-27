b0e4ab937b8049de8960ce15fabc9ab10519f756
ipkpjersi <33754783+ipkpjersi@users.noreply.github.com>
2019-01-27 18:39:49 -0500

    Add git install to clone script

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -5,12 +5,13 @@ GREEN=`tput setaf 2`
 NC=`tput sgr0` # No Color
 
 # Open RSC: A replica RSC private server framework
-
+sudo apt-get install git -y
 clear
 
 echo ""
 echo "${RED}Open RSC${NC}"
 echo "An easy to use RSC private server framework"
 echo ""
+
 git clone https://github.com/Open-RSC/Game.git
 cd Game && sudo chmod +x scripts/*.sh && sudo chmod -R 777 . && "./Go-Linux.sh"
