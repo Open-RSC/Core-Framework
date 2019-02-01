b3e719161cfb2f7818eae30ad8c42b308e637ccf
@Kenix3 <kenixwhisperwind@gmail.com>
2019-02-01 08:59:24 -0500

    Quick bank command now allows admins to show any player the bank screen
    Resolves #1591

--- server/plugins/com/openrsc/server/plugins/commands/Admins.java
+++ server/plugins/com/openrsc/server/plugins/commands/Admins.java
@@ -1024,7 +1024,7 @@ public final class Admins implements CommandListener {
 				return;
 			}
 			// Show bank screen to yourself
-			if (p.getUsernameHash() == player.getUsernameHash()) {
+			if (p.getUsernameHash() == player.getUsernameHash() || cmd.equalsIgnoreCase("quickbank")) {
 				player.setAccessingBank(true);
 				ActionSender.showBank(player);
 			} else {
