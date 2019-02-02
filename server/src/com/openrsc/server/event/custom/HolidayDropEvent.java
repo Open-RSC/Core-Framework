7d4205dcd8ba69d4a06ca8f918f3bea4349199b8
@Kenix3 <kenixwhisperwind@gmail.com>
2019-02-02 01:11:48 -0500

    Refactored global holiday drop logic into a class
    Additionally, messages go to all logged in admins
    Resolves #1595

---
+++ server/src/com/openrsc/server/event/custom/HolidayDropEvent.java
@@ -0,0 +1,80 @@
+package com.openrsc.server.event.custom;
+
+import com.openrsc.server.Constants;
+import com.openrsc.server.model.Point;
+import com.openrsc.server.model.ViewArea;
+import com.openrsc.server.model.entity.GroundItem;
+import com.openrsc.server.model.entity.player.Player;
+import com.openrsc.server.model.world.World;
+import com.openrsc.server.net.rsc.ActionSender;
+import com.openrsc.server.util.rsc.DataConversions;
+import com.openrsc.server.util.rsc.MessageType;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+
+/****
+ * Author: Kenix
+ */
+
+public class HolidayDropEvent extends HourlyEvent  {
+	private ArrayList<Integer> items;
+	private String eventMessage;
+	private Player executor;
+
+	public HolidayDropEvent(int lifeTime, Player executor, ArrayList<Integer> items) {
+		this(lifeTime, executor, items, (String)null);
+	}
+
+	public HolidayDropEvent(int lifeTime, Player executor, ArrayList<Integer> items, String eventMessage) {
+		super(lifeTime);
+		this.executor = executor;
+		this.items = items;
+		this.eventMessage = eventMessage;
+	}
+
+	public void action() {
+		int totalItemsDropped = 0;
+
+		// TODO: This should not require a passed in player.
+		ViewArea view = executor.getViewArea();
+
+		for (int y = 96; y < 870; ) { // Highest Y is 867 currently.
+			for (int x = 1; x < 770; ) { // Highest X is 766 currently.
+
+				// Check for item dropped right beside this
+				if (view.getGroundItem(Point.location(x, y - 1)) == null &&
+					view.getGroundItem(Point.location(x - 1, y - 1)) == null &&
+					view.getGroundItem(Point.location(x + 1, y - 1)) == null) {
+
+					boolean containsObject = view.getGameObject(Point.location(x, y)) != null;
+					int traversal = World.getWorld().getTile(x, y).traversalMask;
+					boolean isBlocking = (
+						(traversal & 16) != 0 || // diagonal wall \
+							(traversal & 32) != 0 || // diagonal wall /
+							(traversal & 64) != 0    // water or black,  etc.
+					);
+					if (!containsObject && !isBlocking) { // Nothing in the way.
+						World.getWorld().registerItem(new GroundItem(items.get(DataConversions.random(0, items.size() - 1)), x, y, 1, null));
+						totalItemsDropped++;
+					}
+				}
+				x += DataConversions.random(20, 27); // How much space between each along X axis
+			}
+			y += DataConversions.random(1, 2);
+		}
+
+		for (Player p : World.getWorld().getPlayers()) {
+			if(!p.isAdmin()) {
+				continue;
+			}
+
+			p.playerServerMessage(MessageType.QUEST, Constants.GameServer.MESSAGE_PREFIX + "Dropped " + totalItemsDropped + " of item IDs: " + StringUtils.join(items, ", "));
+		}
+
+		if(eventMessage != null) {
+			for (Player p : World.getWorld().getPlayers())
+				ActionSender.sendMessage(p, null, 0, MessageType.QUEST, eventMessage, 0);
+		}
+	}
+}
