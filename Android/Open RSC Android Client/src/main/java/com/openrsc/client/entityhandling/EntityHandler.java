fdeedde9f194a7f55016699a3278765acbe3445e
Marwolf <cleako@gmail.com>
2019-02-03 17:13:32 -0500

    Optimizes Android client imports

--- Android/Open RSC Android Client/src/main/java/com/openrsc/client/entityhandling/EntityHandler.java
+++ Android/Open RSC Android Client/src/main/java/com/openrsc/client/entityhandling/EntityHandler.java
@@ -1,12 +1,20 @@
 package com.openrsc.client.entityhandling;
 
-import com.openrsc.client.entityhandling.defs.*;
+import com.openrsc.client.entityhandling.defs.DoorDef;
+import com.openrsc.client.entityhandling.defs.ElevationDef;
+import com.openrsc.client.entityhandling.defs.GameObjectDef;
+import com.openrsc.client.entityhandling.defs.ItemDef;
+import com.openrsc.client.entityhandling.defs.NPCDef;
+import com.openrsc.client.entityhandling.defs.PrayerDef;
+import com.openrsc.client.entityhandling.defs.SpellDef;
+import com.openrsc.client.entityhandling.defs.TileDef;
 import com.openrsc.client.entityhandling.defs.extras.AnimationDef;
 import com.openrsc.client.entityhandling.defs.extras.TextureDef;
-import orsc.Config;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+
+import orsc.Config;
 
 public class EntityHandler {
 
