fb0819bac9bfa042125ad7980392528895c22524
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2019-02-22 18:54:58 +0400

    Agility courses reward full xp regardless of order done, as long as all obstacles were done correctly

--- server/plugins/com/openrsc/server/plugins/npcs/ardougne/east/Gunnjorn.java
+++ server/plugins/com/openrsc/server/plugins/npcs/ardougne/east/Gunnjorn.java
@@ -6,29 +6,23 @@ import com.openrsc.server.plugins.listeners.action.TalkToNpcListener;
 import com.openrsc.server.plugins.listeners.executive.TalkToNpcExecutiveListener;
 
 import static com.openrsc.server.plugins.Functions.npcTalk;
-import static com.openrsc.server.plugins.Functions.showMenu;
+
+import com.openrsc.server.external.NpcId;
 
 public class Gunnjorn implements TalkToNpcListener, TalkToNpcExecutiveListener {
 
 	@Override
 	public boolean blockTalkToNpc(Player p, Npc n) {
-		if (n.getID() == 588) {
-			return true;
-		}
-		return false;
+		return n.getID() == NpcId.GUNNJORN.id();
 	}
 
 	@Override
 	public void onTalkToNpc(Player p, Npc n) {
-		if (n.getID() == 588) {
-			npcTalk(p, n, "Ahoy there!");
-			int menu = showMenu(p, n, "What is this place?");
-			if (menu == 0) {
-				npcTalk(p, n, "Haha welcome to my obstacle course",
+		if (n.getID() == NpcId.GUNNJORN.id()) {
+			npcTalk(p, n, "Haha welcome to my obstacle course",
 					"Have fun, but remember this isn't a child's playground",
 					"People have died here", "The best way to train",
 					"Is to go round the course in a clockwise direction");
-			}
 		}
 	}
 }
--- server/plugins/com/openrsc/server/plugins/skills/agility/WildernessAgilityCourse.java
+++ server/plugins/com/openrsc/server/plugins/skills/agility/WildernessAgilityCourse.java
@@ -8,6 +8,10 @@ import com.openrsc.server.plugins.listeners.executive.ObjectActionExecutiveListe
 
 import static com.openrsc.server.plugins.Functions.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class WildernessAgilityCourse implements ObjectActionListener,
 	ObjectActionExecutiveListener {
 
@@ -18,6 +22,9 @@ public class WildernessAgilityCourse implements ObjectActionListener,
 	private static final int STONE = 707;
 	private static final int LEDGE = 708;
 	private static final int VINE = 709;
+	
+	private static Set<Integer> obstacles = new HashSet<Integer>(Arrays.asList(WILD_PIPE, WILD_ROPESWING, STONE, LEDGE));
+	private static Integer lastObstacle = VINE;
 
 	@Override
 	public boolean blockObjectAction(GameObject obj, String command, Player p) {
@@ -26,55 +33,63 @@ public class WildernessAgilityCourse implements ObjectActionListener,
 
 	@Override
 	public void onObjectAction(GameObject obj, String command, Player p) {
+		int failRate;
+		if (obj.getID() == GATE) {
+			if (getCurrentLevel(p, Skills.AGILITY) < 52) {
+				p.message("You need an agility level of 52 to attempt balancing along the ridge");
+				p.setBusy(false);
+				return;
+			}
+			p.message("You go through the gate and try to edge over the ridge");
+			sleep(1000);
+			movePlayer(p, 298, 130);
+			failRate = failRate();
+			if (failRate == 1) {
+				message(p, "you lose your footing and fall into the wolf pit");
+				movePlayer(p, 300, 129);
+			} else if (failRate == 2) {
+				message(p, "you lose your footing and fall into the wolf pit");
+				movePlayer(p, 296, 129);
+			} else {
+				message(p, "You skillfully balance across the ridge");
+				movePlayer(p, 298, 125);
+				p.incExp(Skills.AGILITY, 50, true);
+			}
+			return;
+		}
+		else if (obj.getID() == SECOND_GATE) {
+			p.message("You go through the gate and try to edge over the ridge");
+			sleep(1000);
+			movePlayer(p, 298, 130);
+			sleep(1000);
+			failRate = failRate();
+			if (failRate == 1) {
+				message(p, "you lose your footing and fall into the wolf pit");
+				movePlayer(p, 300, 129);
+
+			} else if (failRate == 2) {
+				message(p, "you lose your footing and fall into the wolf pit");
+				movePlayer(p, 296, 129);
+			} else {
+				message(p, "You skillfully balance across the ridge");
+				movePlayer(p, 298, 134);
+				p.incExp(Skills.AGILITY, 50, true);
+			}
+			return;
+		}
+		if (p.getFatigue() >= p.MAX_FATIGUE && !inArray(obj.getID(), WILD_PIPE, WILD_ROPESWING, STONE, LEDGE)) {
+			p.message("you are too tired to train");
+			return;
+		}
 		p.setBusy(true);
 		boolean failCourse = failWildCourse(p);
 		switch (obj.getID()) {
-			case GATE:
-				if (getCurrentLevel(p, Skills.AGILITY) < 52) {
-					p.message("You need an agility level of 52 to attempt balancing along the ridge");
-					p.setBusy(false);
-					return;
-				}
-				p.message("You go through the gate and try to edge over the ridge");
-				sleep(1000);
-				movePlayer(p, 298, 130);
-				int failRate = failRate();
-				if (failRate == 1) {
-					message(p, "you lose your footing and fall into the wolf pit");
-					movePlayer(p, 300, 129);
-				} else if (failRate == 2) {
-					message(p, "you lose your footing and fall into the wolf pit");
-					movePlayer(p, 296, 129);
-				} else {
-					message(p, "You skillfully balance across the ridge");
-					movePlayer(p, 298, 125);
-					p.incExp(Skills.AGILITY, 50, true);
-				}
-				break;
-			case SECOND_GATE:
-				p.message("You go through the gate and try to edge over the ridge");
-				sleep(1000);
-				movePlayer(p, 298, 130);
-				sleep(1000);
-				failRate = failRate();
-				if (failRate == 1) {
-					message(p, "you lose your footing and fall into the wolf pit");
-					movePlayer(p, 300, 129);
-
-				} else if (failRate == 2) {
-					message(p, "you lose your footing and fall into the wolf pit");
-					movePlayer(p, 296, 129);
-				} else {
-					message(p, "You skillfully balance across the ridge");
-					movePlayer(p, 298, 134);
-					p.incExp(Skills.AGILITY, 50, true);
-				}
-				break;
 			case WILD_PIPE:
 				p.message("You squeeze through the pipe");
 				sleep(1000);
 				movePlayer(p, 294, 112);
 				p.incExp(Skills.AGILITY, 50, true);
+				AgilityUtils.completedObstacle(p, obj.getID(), obstacles, lastObstacle, 1500);
 				break;
 			case WILD_ROPESWING:
 				p.message("You grab the rope and try and swing across");
@@ -84,19 +99,20 @@ public class WildernessAgilityCourse implements ObjectActionListener,
 					message(p, "You skillfully swing across the hole");
 					movePlayer(p, 292, 108);
 					p.incExp(Skills.AGILITY, 100, true);
+					AgilityUtils.completedObstacle(p, obj.getID(), obstacles, lastObstacle, 1500);
 				} else { // 13 damage on 85hp.
 					// 11 damage on 73hp.
 					//
 					p.message("Your hands slip and you fall to the level below");
 					sleep(1000);
 					movePlayer(p, 293, 2942);
-					p.message("you land painfully on the spikes");
+					p.message("You land painfully on the spikes");
 					playerTalk(p, null, "ouch");
 					p.damage(damage);
 				}
 				break;
 			case STONE:
-				p.message("You stand on the stepping stones");
+				p.message("you stand on the stepping stones");
 				sleep(1000);
 				if (failCourse) {
 					movePlayer(p, 293, 105);
@@ -108,10 +124,11 @@ public class WildernessAgilityCourse implements ObjectActionListener,
 					sleep(600);
 					movePlayer(p, 296, 105);
 					sleep(600);
-					movePlayer(p, 297, 105);
+					movePlayer(p, 297, 106);
 					p.incExp(Skills.AGILITY, 80, true);
+					AgilityUtils.completedObstacle(p, obj.getID(), obstacles, lastObstacle, 1500);
 				} else {
-					p.message("You lose your footing and land in the lava");
+					p.message("Your lose your footing and land in the lava");
 					movePlayer(p, 292, 104);
 					int lavaDamage = (int) Math.round((p.getSkills().getLevel(Skills.HITPOINTS)) * 0.21D);
 					p.damage(lavaDamage);
@@ -135,23 +152,25 @@ public class WildernessAgilityCourse implements ObjectActionListener,
 					sleep(600);
 					movePlayer(p, 301, 111);
 					p.incExp(Skills.AGILITY, 80, true);
+					AgilityUtils.completedObstacle(p, obj.getID(), obstacles, lastObstacle, 1500);
 				} else {
 					p.message("you lose your footing and fall to the level below");
 					sleep(1000);
 					movePlayer(p, 298, 2945);
-					p.message("you land painfully on the spikes");
+					p.message("You land painfully on the spikes");
 					playerTalk(p, null, "ouch");
 					p.damage(ledgeDamage);
 				}
 				break;
 			case VINE:
-				p.message("you climb up the cliff");
+				p.message("You climb up the cliff");
 				movePlayer(p, 305, 118);
 				sleep(600);
 				movePlayer(p, 304, 119);
 				sleep(600);
 				movePlayer(p, 304, 120);
-				p.incExp(Skills.AGILITY, 1580, true); // COMPLETION OF THE COURSE.
+				p.incExp(Skills.AGILITY, 80, true); // COMPLETION OF THE COURSE.
+				AgilityUtils.completedObstacle(p, obj.getID(), obstacles, lastObstacle, 1500);
 				break;
 		}
 		p.setBusy(false);
