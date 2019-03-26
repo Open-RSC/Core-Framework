220a2c2c52bfcda0df59d17b8d33fe7a0a0256d1
Marwolf <cleako@gmail.com>
2019-03-26 01:44:49 -0400

    Updates launcher version

--- Launcher/src/com/loader/openrsc/Constants.java
+++ Launcher/src/com/loader/openrsc/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
 	public static final String CONF_DIR = "Cache";
 	public static final String CLIENT_FILENAME = "Open_RSC_Client.jar";
 	public static final String CACHE_URL = BASE_URL + "downloads/cache/";
-	public static final Double VERSION_NUMBER = 20190325.160000; //YYYYMMDD.HHMMSS format
+	public static final Double VERSION_NUMBER = 20190326.014400; //YYYYMMDD.HHMMSS format
 	public static final String VERSION_UPDATE_URL = "https://gitlab.openrsc.com/open-rsc/Game/raw/2.0.0/Launcher/src/com/loader/openrsc/Constants.java";
 	public static final String UPDATE_JAR_URL = "https://game.openrsc.com/downloads/OpenRSC.jar";
 	public static final String JAR_FILENAME = "OpenRSC.jar";
