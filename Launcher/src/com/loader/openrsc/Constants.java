ba5577e79dc4d2ebaa6238712b456b4b7b0dca1a
Marwolf <cleako@gmail.com>
2019-05-12 22:59:25 -0400

    Sets launcher to read Cabbage stats

--- Launcher/src/com/loader/openrsc/Constants.java
+++ Launcher/src/com/loader/openrsc/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 
 	// RSC Cabbage
 	public static final String RSCC_GAME_NAME = "RSC Cabbage";
-	public static final String RSCC_WORLD_STATS_URL = "https://cabbage.openrsc.com/stats"; // Used for the world statistics
+	public static final String RSCC_WORLD_STATS_URL = "https://cabbage.openrsc.com/cabbagestats"; // Used for the world statistics
 	static final String RSCC_SERVER_DOMAIN = "cabbage.openrsc.com"; // Used for the server status display
 	static final int RSCC_SERVER_PORT = 43595;
 
@@ -32,7 +32,7 @@ public class Constants {
 	public static final String CONF_DIR = "Cache";
 	public static final String CLIENT_FILENAME = "Open_RSC_Client.jar";
 	public static final String CACHE_URL = BASE_URL + "downloads/cache/";
-	public static final Double VERSION_NUMBER = 20190512.171200; //YYYYMMDD.HHMMSS format
+	public static final Double VERSION_NUMBER = 20190512.225700; //YYYYMMDD.HHMMSS format
 	public static final String VERSION_UPDATE_URL = "https://gitlab.openrsc.com/open-rsc/Game/raw/2.0.0/Launcher/src/com/loader/openrsc/Constants.java";
 	public static final String UPDATE_JAR_URL = "https://game.openrsc.com/downloads/OpenRSC.jar";
 	public static final String JAR_FILENAME = "OpenRSC.jar";
