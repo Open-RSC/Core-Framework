1d506c4f6c239ff3d2786e37673d5fe56dea0650
Marwolf <cleako@gmail.com>
2019-05-21 02:42:39 -0400

    Removes unneeded code

--- server/src/com/openrsc/server/net/rsc/handlers/LogoutRequest.java
+++ server/src/com/openrsc/server/net/rsc/handlers/LogoutRequest.java
@@ -14,11 +14,6 @@ public class LogoutRequest implements PacketHandler {
 
 	public void handlePacket(Packet p, Player player) throws Exception {
 		if (player.canLogout()) {
-			// returns a single pet crystal if the player logs out without returning it
-			/*if (Constants.GameServer.WANT_PETS && player.getInventory().hasItemId(ItemId.A_GLOWING_RED_CRYSTAL.id())) {
-				removeItem(player, ItemId.A_GLOWING_RED_CRYSTAL.id(), 1);
-				addItem(player, ItemId.A_RED_CRYSTAL.id(), 1);
-			}*/
 			player.unregister(false, "Player requested log out");
 		} else {
 			ActionSender.sendCantLogout(player);
