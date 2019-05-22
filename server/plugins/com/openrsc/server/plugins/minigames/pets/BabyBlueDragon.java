67ffd69359cca87e617dbdd8b9fc36f02d2278bb
Marwolf <cleako@gmail.com>
2019-05-22 12:14:06 -0400

    Reimplements pet code for baby blue dragons

--- server/plugins/com/openrsc/server/plugins/minigames/pets/BabyBlueDragon.java
+++ server/plugins/com/openrsc/server/plugins/minigames/pets/BabyBlueDragon.java
@@ -8,13 +8,16 @@ import com.openrsc.server.external.NpcId;
 import com.openrsc.server.model.container.Item;
 import com.openrsc.server.model.entity.npc.Npc;
 import com.openrsc.server.model.entity.player.Player;
+import com.openrsc.server.net.rsc.ActionSender;
 import com.openrsc.server.plugins.listeners.action.InvUseOnNpcListener;
 import com.openrsc.server.plugins.listeners.executive.InvUseOnNpcExecutiveListener;
 
-import static com.openrsc.server.plugins.Functions.attack;
+import static com.openrsc.server.plugins.Functions.addItem;
 import static com.openrsc.server.plugins.Functions.getMultipleNpcsInArea;
 import static com.openrsc.server.plugins.Functions.message;
 import static com.openrsc.server.plugins.Functions.npcYell;
+import static com.openrsc.server.plugins.Functions.random;
+import static com.openrsc.server.plugins.Functions.removeItem;
 import static com.openrsc.server.plugins.Functions.showBubble;
 
 public class BabyBlueDragon implements InvUseOnNpcListener, InvUseOnNpcExecutiveListener {
@@ -58,12 +61,11 @@ public class BabyBlueDragon implements InvUseOnNpcListener, InvUseOnNpcExecutive
 							npcYell(player, nearbyNpc, "grrrarrr!");
 						}
 						message(player, 1300, "The nearby " + (nearbyNpc.getDef().getName().contains("dragon") ? nearbyNpc.getDef().getName() : "" + nearbyNpc.getDef().getName().toLowerCase()) + " take a sudden dislike to you.");
-						//nearbyNpc.setChasing(player);
+						nearbyNpc.setChasing(player);
 						//transform(nearbyNpc, 11, true);
-						attack(npc, nearbyNpc);
-						//attack(nearbyNpc, npc);
+						//attack(npc, nearbyNpc);
 					}
-					/*if (random(0, 4) != 0) {
+					if (random(0, 4) != 0) {
 						player.message("You catch the baby blue dragon in the crystal.");
 						removeItem(player, ItemId.A_GLOWING_RED_CRYSTAL.id(), 1);
 						addItem(player, ItemId.A_RED_CRYSTAL.id(), 1);
@@ -75,7 +77,7 @@ public class BabyBlueDragon implements InvUseOnNpcListener, InvUseOnNpcExecutive
 						player.message("The baby blue dragon manages to get away from you!");
 						npc.setBusyTimer(0);
 						player.setBusy(false);
-					}*/
+					}
 				}
 			});
 		}
