fc36daaa086ef0edf6753dc3b80e79ffc5822211
Joe K <xjoe282828@hotmail.com>
2019-06-29 01:10:23 -0400

    Kill feed fix
    This commit reverts a previous commit which caused the killfeed to not function properly.

--- client/src/orsc/graphics/gui/KillAnnouncer.java
+++ client/src/orsc/graphics/gui/KillAnnouncer.java
@@ -1,26 +1,20 @@
 package orsc.graphics.gui;
 
 public class KillAnnouncer {
-
+	
+	public String killString,killerString,killedString;
 	public int killPicture;
-	public String killerString;
-	long displayTime;
-	private String killedString;
-
+	public long displayTime;
+	
 	public KillAnnouncer(String kill) {
+		killString = kill;
 		displayTime = System.currentTimeMillis();
 	}
 
 	public KillAnnouncer(String killer, String killed, int killType) {
+		killerString = killer;
+		killedString = killed;
 		killPicture = killType;
 		displayTime = System.currentTimeMillis();
-	}
-
-	public String getKilledString() {
-		return killedString;
-	}
-
-	public void setKilledString(String killedString) {
-		this.killedString = killedString;
 	}
 }
