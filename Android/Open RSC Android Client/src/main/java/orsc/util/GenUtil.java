8b1dd0ca4a374aa701a898b63073c62b02eeb099
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2019-07-22 21:22:35 -0400

    Fallbacks on trying to make ip/port etc files for Android users

--- Android/Open RSC Android Client/src/main/java/orsc/util/GenUtil.java
+++ Android/Open RSC Android Client/src/main/java/orsc/util/GenUtil.java
@@ -8,6 +8,15 @@ public class GenUtil {
 	private static long lastTimeCall;
 	private static long timeOverflow;
 
+	public static void close(Closeable c) {
+		if (c == null) return;
+		try {
+			c.close();
+		} catch (IOException e) {
+			//log the exception
+		}
+	}
+
 	public static InputStream chooseStreamFor(String file) throws IOException {
 		try {
 			InputStream stream;
