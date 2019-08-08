e2acb870dc7a9edd062b3be0e5bd497346da6601
Marwolf <marwood.wolf@gmail.com>
2019-08-08 14:55:02 -0700

    Adds missing primary key to bank presets table

--- Databases/cabbage_game_players.sql
+++ Databases/cabbage_game_players.sql
@@ -81,10 +81,12 @@ CREATE TABLE IF NOT EXISTS `openrsc_bank` (
 
 DROP TABLE IF EXISTS `openrsc_bankpresets`;
 CREATE TABLE IF NOT EXISTS `openrsc_bankpresets` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `playerID` int(10) unsigned NOT NULL,
   `slot` int(10) unsigned NOT NULL,
   `inventory` blob DEFAULT NULL,
-  `equipment` blob DEFAULT NULL
+  `equipment` blob DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `openrsc_chat_logs`;
