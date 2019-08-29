db50fa85475f3acdab1e40f9eb1f68420f0afbc9
Kenix <kenixwhisperwind@gmail.com>
2019-08-29 07:38:13 -0400

    Revert "Converted Prayer sleep to a GameStateEvent"
    This reverts commit e5f5eb763fc226d0d15e5286ff16977cd7756f95.

--- server/plugins/com/openrsc/server/plugins/skills/Prayer.java
+++ server/plugins/com/openrsc/server/plugins/skills/Prayer.java
@@ -1,46 +1,38 @@
 package com.openrsc.server.plugins.skills;
 
 import com.openrsc.server.constants.Skills;
-import com.openrsc.server.event.rsc.GameStateEvent;
 import com.openrsc.server.model.entity.GameObject;
 import com.openrsc.server.model.entity.player.Player;
 import com.openrsc.server.plugins.listeners.action.ObjectActionListener;
 import com.openrsc.server.plugins.listeners.executive.ObjectActionExecutiveListener;
 
-import static com.openrsc.server.plugins.Functions.*;
+import static com.openrsc.server.plugins.Functions.getCurrentLevel;
+import static com.openrsc.server.plugins.Functions.getMaxLevel;
+import static com.openrsc.server.plugins.Functions.message;
+import static com.openrsc.server.plugins.Functions.sleep;
 
 public class Prayer implements ObjectActionExecutiveListener, ObjectActionListener {
 
 	@Override
 	public void onObjectAction(final GameObject object, String command, Player player) {
-		player.getWorld().getServer().getGameEventHandler().add(new GameStateEvent(player.getWorld(), player, 0, "Prayer Recharge") {
-			public void init() {
-				addState(0, () -> {
-					if (command.equalsIgnoreCase("recharge at")) {
-						int maxPray = getMaxLevel(getPlayerOwner(), Skills.PRAYER) + (object.getID() == 200 ? 2 : 0);
-						if (getCurrentLevel(getPlayerOwner(), Skills.PRAYER) == maxPray) {
-							getPlayerOwner().message("You already have full prayer points");
-						} else {
-							getPlayerOwner().message("You recharge your prayer points");
-							getPlayerOwner().playSound("recharge");
-							if (getCurrentLevel(getPlayerOwner(), Skills.PRAYER) < maxPray) {
-								getPlayerOwner().getSkills().setLevel(Skills.PRAYER, maxPray);
-							}
+		if (command.equalsIgnoreCase("recharge at")) {
+			int maxPray = getMaxLevel(player, Skills.PRAYER) + (object.getID() == 200 ? 2 : 0);
+			if (getCurrentLevel(player, Skills.PRAYER) == maxPray) {
+				player.message("You already have full prayer points");
+			} else {
+				player.message("You recharge your prayer points");
+				player.playSound("recharge");
+				if (getCurrentLevel(player, Skills.PRAYER) < maxPray) {
+					player.getSkills().setLevel(Skills.PRAYER, maxPray);
+				}
 
-						}
-						if (object.getID() == 625 && object.getY() == 3573) {
-							return invoke(1, 1);
-						}
-					}
-					return null;
-				});
-				addState(1, () -> {
-					message(getPlayerOwner(), "Suddenly a trapdoor opens beneath you");
-					getPlayerOwner().teleport(608, 3525);
-					return null;
-				});
 			}
-		});
+			if (object.getID() == 625 && object.getY() == 3573) {
+				sleep(650);
+				message(player, "Suddenly a trapdoor opens beneath you");
+				player.teleport(608, 3525);
+			}
+		}
 	}
 
 	@Override
