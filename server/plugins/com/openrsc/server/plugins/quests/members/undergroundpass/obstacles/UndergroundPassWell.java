d02421e941241e209a3ad9b70fe7205fb61cba37
Kenix <kenixwhisperwind@gmail.com>
2019-08-29 08:06:08 -0400

    Converted UndergroundPassWell sleep to a GameStateEvent

--- server/plugins/com/openrsc/server/plugins/quests/members/undergroundpass/obstacles/UndergroundPassWell.java
+++ server/plugins/com/openrsc/server/plugins/quests/members/undergroundpass/obstacles/UndergroundPassWell.java
@@ -2,6 +2,7 @@ package com.openrsc.server.plugins.quests.members.undergroundpass.obstacles;
 
 import com.openrsc.server.constants.Quests;
 import com.openrsc.server.constants.Skills;
+import com.openrsc.server.event.rsc.GameStateEvent;
 import com.openrsc.server.model.entity.GameObject;
 import com.openrsc.server.model.entity.player.Player;
 import com.openrsc.server.plugins.listeners.action.ObjectActionListener;
@@ -20,22 +21,32 @@ public class UndergroundPassWell implements ObjectActionListener, ObjectActionEx
 
 	@Override
 	public void onObjectAction(GameObject obj, String command, Player p) {
-		if (obj.getID() == WELL) {
-			message(p, "you climb into the well");
-			if ((p.getCache().hasKey("orb_of_light1") && p.getCache().hasKey("orb_of_light2") && p.getCache().hasKey("orb_of_light3") && p.getCache().hasKey("orb_of_light4")) ||
-					atQuestStages(p, Quests.UNDERGROUND_PASS, 7, 8, -1)) {
-				message(p, "you feel the grip of icy hands all around you...");
-				p.teleport(722, 3461);
-				sleep(600);
-				displayTeleportBubble(p, p.getX(), p.getY(), true);
-				p.message("..slowly dragging you futher down into the caverns");
-			} else {
-				p.damage((int) (getCurrentLevel(p, Skills.HITS) * 0.2D));
-				displayTeleportBubble(p, obj.getX(), obj.getY(), false);
-				message(p, "from below an icy blast of air chills you to your bones",
-					"a mystical force seems to blast you back out of the well");
-				p.message("there must be a positive force near by!");
+		p.getWorld().getServer().getGameEventHandler().add(new GameStateEvent(p.getWorld(), p, 0, "Underground Pass Well") {
+			public void init() {
+				addState(0, () -> {
+					if (obj.getID() == WELL) {
+						message(p, "you climb into the well");
+						if ((p.getCache().hasKey("orb_of_light1") && p.getCache().hasKey("orb_of_light2") && p.getCache().hasKey("orb_of_light3") && p.getCache().hasKey("orb_of_light4")) ||
+							atQuestStages(p, Quests.UNDERGROUND_PASS, 7, 8, -1)) {
+							message(p, "you feel the grip of icy hands all around you...");
+							p.teleport(722, 3461);
+							return invoke(1, 1);
+						} else {
+							p.damage((int) (getCurrentLevel(p, Skills.HITS) * 0.2D));
+							displayTeleportBubble(p, obj.getX(), obj.getY(), false);
+							message(p, "from below an icy blast of air chills you to your bones",
+								"a mystical force seems to blast you back out of the well");
+							p.message("there must be a positive force near by!");
+						}
+					}
+					return null;
+				});
+				addState(1, () -> {
+					displayTeleportBubble(p, p.getX(), p.getY(), true);
+					p.message("..slowly dragging you futher down into the caverns");
+					return null;
+				});
 			}
-		}
+		});
 	}
 }
