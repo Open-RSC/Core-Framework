f7b97d1b53f5043bda1d3cbd419f6f8e69ff5e6a
Marwolf <marwood.wolf@gmail.com>
2019-10-07 09:52:53 -0400

    Android version 67

--- Android/Open RSC Android Client/src/main/java/orsc/Config.java
+++ Android/Open RSC Android Client/src/main/java/orsc/Config.java
@@ -46,8 +46,8 @@ public class Config {
 	public static boolean F_ANDROID_BUILD = true; // This MUST be true if Android client or it will crash on launch, needs to be set as public for the Android client to use
 	public static final String DL_URL = "androidcheck.openrsc.com"; // needs to be set as public for the Android client to use
 	public static final String ANDROID_DOWNLOAD_PATH = "https://" + DL_URL + "/downloads/";
-	public static final String CACHE_URL = "https://" + DL_URL + "/downloads/cache/";
-	public static final int ANDROID_CLIENT_VERSION = 66; // Important! Depends on web server android_version.txt to check for an updated version
+	public static final String CACHE_URL = "https://" + DL_URL + "/downloads/";
+	public static final int ANDROID_CLIENT_VERSION = 67; // Important! Depends on web server android_version.txt to check for an updated version
 	public static boolean F_SHOWING_KEYBOARD = false;
 	public static boolean C_HOLD_AND_CHOOSE = true;
 	public static int C_LONG_PRESS_TIMER = 5; // default hold timer setting
