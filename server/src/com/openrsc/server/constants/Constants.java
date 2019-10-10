8494d66722d02c7c641c1eb81bde226a785e3ec5
lmsv-mx123 <salazar.luis.miguel@gmail.com>
2019-10-10 19:05:50 -0400

    Changed retreat calculation to authentic retreat

--- server/src/com/openrsc/server/constants/Constants.java
+++ server/src/com/openrsc/server/constants/Constants.java
@@ -7,6 +7,7 @@ public final class Constants {
 	private final Server server;
 	private final com.openrsc.server.constants.Skills skills;
 	private final Poison poison;
+	private final Retreats retreats;
 	private final Minigames minigames;
 	private final Quests quests;
 
@@ -14,6 +15,7 @@ public final class Constants {
 		this.server = server;
 		this.skills = new com.openrsc.server.constants.Skills(this);
 		this.poison = new Poison(this);
+		this.retreats = new Retreats(this);
 		this.minigames = new Minigames(this);
 		this.quests = new Quests(this);
 	}
@@ -26,6 +28,9 @@ public final class Constants {
 	}
 	public final Poison getPoison() {
 		return poison;
+	}
+	public final Retreats getRetreats() {
+		return retreats;
 	}
 	public final Minigames getMinigames() {
 		return minigames;
@@ -44,27 +49,6 @@ public final class Constants {
 	 */
 	public final int[][] SPELLS = {{1, 1}, {5, 2}, {9, 3}, {13, 4}, {17, 5}, {23, 5}, {29, 6}, {35, 6}, {41, 7}, {47, 7}, {53, 8}, {59, 8}, {62, 9}, {65, 9}, {70, 10}, {75, 10}};
 
-	/**
-	 * Normal list
-	 * */
-	public final int[] NPCS_THAT_RETREAT_NORM = {NpcId.CHICKEN.id(), NpcId.IMP.id(), NpcId.UNICORN.id(), NpcId.BLACK_UNICORN.id(),
-		NpcId.GOBLIN_OBSERVATORY.id(), NpcId.DUNGEON_RAT.id(), NpcId.HIGHWAYMAN.id(), NpcId.BEAR_LVL24.id(), NpcId.BEAR_LVL26.id(),
-		NpcId.UGTHANKI.id(), NpcId.SPIDER.id(), NpcId.RAT_WITCHES_POTION.id(), NpcId.THIEF_GENERIC.id(), NpcId.THIEF_BLANKET.id(),
-		NpcId.MUGGER.id(), NpcId.SCORPION.id(), NpcId.GIANT_SPIDER_LVL8.id(), NpcId.RAT_LVL8.id(), NpcId.ROGUE.id(),
-		NpcId.OOMLIE_BIRD.id(), NpcId.FIREBIRD.id(), NpcId.COW_ATTACKABLE.id(), NpcId.CHAOS_DWARF.id(),
-		NpcId.MONK.id(), NpcId.SHANTAY_PASS_GUARD_MOVING.id(), NpcId.FORESTER.id(), NpcId.ROWDY_GUARD.id(),
-		NpcId.ROWDY_SLAVE.id(), NpcId.WIZARD.id(), NpcId.ZOMBIE_LVL19.id(), NpcId.SOULESS_UNDEAD.id()
-	};
-	/**
-	 * List for extremely low health 5% - min 1hp
-	 * */
-	public final int[] NPCS_THAT_RETREAT_LOW = {NpcId.ZOMBIE_LVL24_GEN.id(), NpcId.SHADOW_SPIDER.id(), NpcId.DEADLY_RED_SPIDER.id(),
-		NpcId.ICE_SPIDER.id(), NpcId.JUNGLE_SPIDER.id(), NpcId.GIANT_SPIDER_LVL31.id(), NpcId.POISON_SCORPION.id(),
-		NpcId.KING_SCORPION.id(), NpcId.DONNY_THE_LAD.id(), NpcId.SPEEDY_KEITH.id(), NpcId.BLACK_HEATHER.id(), NpcId.ZOMBIE_INVOKED.id(),
-		NpcId.ZOMBIE_ENTRANA.id(), NpcId.ZOMBIE_LVL32.id(), NpcId.ZOMBIE_WMAZEKEY.id(), NpcId.GIANT_BAT.id(), NpcId.DEATH_WING.id(),
-		NpcId.RAT_LVL13.id(), NpcId.RAT_WMAZEKEY.id(), NpcId.HOBGOBLIN_LVL32.id(), NpcId.OTHERWORLDLY_BEING.id(),
-		NpcId.WYSON_THE_GARDENER.id(), NpcId.STRAVEN.id(), NpcId.JONNY_THE_BEARD.id()
-	};
 	/**
 	 * ID's of all Undead-type of NPC's. (Used for crumble undead & sounds)
 	 */
