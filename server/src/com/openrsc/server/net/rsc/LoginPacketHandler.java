c69bb90383510e3a4de756b94f99b2ed77a9e9eb
Marwolf <marwood.wolf@gmail.com>
2019-10-22 17:12:27 -0400

    Fixed player registration always showing as registered recently
    Fixes #2334

--- server/src/com/openrsc/server/net/rsc/LoginPacketHandler.java
+++ server/src/com/openrsc/server/net/rsc/LoginPacketHandler.java
@@ -90,11 +90,6 @@ public class LoginPacketHandler {
 			case 78:
 				LOGGER.info("Registration attempt from: " + IP);
 
-				if(server.getPacketFilter().shouldAllowLogin(IP, true)) {
-					channel.writeAndFlush(new PacketBuilder().writeByte((byte) 5).toPacket());
-					channel.close();
-				}
-
 				String user = getString(p.getBuffer()).trim();
 				String pass = getString(p.getBuffer()).trim();
 
@@ -103,10 +98,12 @@ public class LoginPacketHandler {
 
 				String email = getString(p.getBuffer()).trim();
 
-				CharacterCreateRequest characterCreateRequest = new CharacterCreateRequest(server, channel, user, pass, email);
-				server.getLoginExecutor().addCharacterCreateRequest(characterCreateRequest);
+				if (server.getPacketFilter().shouldAllowLogin(IP, true)) {
+					CharacterCreateRequest characterCreateRequest = new CharacterCreateRequest(server, channel, user, pass, email);
+					server.getLoginExecutor().addCharacterCreateRequest(characterCreateRequest);
+				}
 				break;
-				
+
 			/* Forgot password */
 			case 5:
 				try {
@@ -118,13 +115,13 @@ public class LoginPacketHandler {
 
 					user = getString(p.getBuffer()).trim();
 					user = user.replaceAll("[^=,\\da-zA-Z\\s]|(?<!,)\\s", " ");
-					
+
 					PreparedStatement statement = server.getDatabaseConnection().prepareStatement("SELECT id FROM " + server.getConfig().MYSQL_TABLE_PREFIX + "players WHERE username=?");
 					statement.setString(1, user);
 					ResultSet res = statement.executeQuery();
 					ResultSet res2 = null;
 					boolean foundAndHasRecovery = false;
-					
+
 					if (res.next()) {
 						statement = server.getDatabaseConnection().prepareStatement("SELECT * FROM " + server.getConfig().MYSQL_TABLE_PREFIX + "player_recovery WHERE playerID=?");
 						statement.setInt(1, res.getInt("id"));
@@ -133,7 +130,7 @@ public class LoginPacketHandler {
 							foundAndHasRecovery = true;
 						}
 					}
-					
+
 					if (!foundAndHasRecovery) {
 						channel.writeAndFlush(new PacketBuilder().writeByte((byte) 0).toPacket());
 						channel.close();
@@ -142,8 +139,8 @@ public class LoginPacketHandler {
 						com.openrsc.server.net.PacketBuilder s = new com.openrsc.server.net.PacketBuilder();
 						String st;
 						for (int n = 0; n < 5; ++n) {
-							st = res2.getString("question"+(n+1));
-							s.writeByte((byte)st.length()+1);
+							st = res2.getString("question" + (n + 1));
+							s.writeByte((byte) st.length() + 1);
 							s.writeString(st);
 						}
 						channel.writeAndFlush(s.toPacket());
@@ -155,7 +152,7 @@ public class LoginPacketHandler {
 					channel.close();
 				}
 				break;
-			
+
 			/* Attempt recover */
 			case 7:
 				user = getString(p.getBuffer()).trim();
@@ -164,7 +161,7 @@ public class LoginPacketHandler {
 				String newPass = getString(p.getBuffer()).trim();
 				Long uid = p.getBuffer().readLong();
 				String answers[] = new String[5];
-				for (int j=0; j<5; j++) {
+				for (int j = 0; j < 5; j++) {
 					answers[j] = DataConversions.normalize(getString(p.getBuffer()).trim(), 50);
 				}
 
