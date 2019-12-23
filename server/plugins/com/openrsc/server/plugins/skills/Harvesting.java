fb42f64460e9832440d610b4c2e569c7f3dbe84a
Kenix <kenixwhisperwind@gmail.com>
2019-12-23 14:34:47 -0500

    Implements command to spawn Christmas Trees

--- server/plugins/com/openrsc/server/plugins/skills/Harvesting.java
+++ server/plugins/com/openrsc/server/plugins/skills/Harvesting.java
@@ -36,7 +36,7 @@ public final class Harvesting implements ObjectActionListener,
 							getOwner().getInventory().add(present);
 							getOwner().playerServerMessage(MessageType.QUEST, "You get a nice looking present");
 						}
-						if (DataConversions.random(1, 1000) <= 3) {
+						if (DataConversions.random(1, 1000) <= 10) {
 							obj = getOwner().getViewArea().getGameObject(object.getID(), object.getX(), object.getY());
 							int depletedId = 1239;
 							interrupt();
