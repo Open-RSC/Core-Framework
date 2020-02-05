3b4b50703eabe7522d4d0457b68efb6f1000636e
Marwolf <marwolf@openrsc.com>
2020-02-04 19:55:13 -0500

    Minor corrections for Android audio playback and sets version to 93

--- Android_Client/Open RSC Android Client/src/main/java/orsc/soundPlayer.java
+++ Android_Client/Open RSC Android Client/src/main/java/orsc/soundPlayer.java
@@ -1,103 +1,36 @@
 package orsc;
 
 import android.media.AudioAttributes;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
 import android.media.MediaPlayer;
-import android.media.SoundPool;
-
-import orsc.mudclient;
-import orsc.util.GenUtil;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class soundPlayer {
-	public static void playSoundFile(String key) {
-		try {
-			if (!mudclient.optionSoundDisabled) {
-				File sound = mudclient.soundCache.get(key + ".wav");
-				if (sound == null)
-					return;
-				try {
-					MediaPlayer player = new MediaPlayer();
-					AudioAttributes audioAttrib = new AudioAttributes.Builder()
-						.setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-						.setUsage(AudioAttributes.USAGE_MEDIA)
-						.build();
-					player.setDataSource(sound.getPath());
-					player.setAudioAttributes(audioAttrib);
-					player.setLooping(false);
-					player.prepare();
-					player.start();
-					player.setOnCompletionListener((mediaPlayer) -> {
-						mediaPlayer.release();
-					});
+    public static void playSoundFile(String key) {
+        try {
+            if (!orsc.mudclient.optionSoundDisabled) {
+                File sound = orsc.mudclient.soundCache.get(key + ".wav");
+                if (sound == null)
+                    return;
+                try {
+                    MediaPlayer player = new MediaPlayer();
+                    AudioAttributes audioAttrib = new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build();
+                    player.setDataSource(sound.getPath());
+                    player.setAudioAttributes(audioAttrib);
+                    player.setLooping(false);
+                    player.prepare();
+                    player.start();
+                    player.setOnCompletionListener(MediaPlayer::release);
 
-					/*
-					Alternative with bare metal AudioTrack, prefered MediaPlayer
-					int i = 0;
-					byte[] music = null;
-					InputStream is = new FileInputStream(sound);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
 
-					AudioAttributes audioAttrib = new AudioAttributes.Builder()
-						.setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-						.setUsage(AudioAttributes.USAGE_MEDIA)
-						.build();
-
-					AudioFormat audioForm = new AudioFormat.Builder()
-						.setSampleRate(8000)
-						.setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-						.build();
-
-					int minBufferSize = AudioTrack.getMinBufferSize(8000,
-						AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-
-					AudioTrack at = new AudioTrack(audioAttrib, audioForm, minBufferSize, AudioTrack.MODE_STREAM, 0);
-
-					music = new byte[512];
-					at.play();
-
-					while((i = is.read(music)) != -1)
-						at.write(music, 0, i);
-
-					at.stop();
-					at.release();
-					is.close();*/
-
-					/*
-					Attempt of using SoundPool but no sound either because file mono not supported
-					or it attempts to play as 44k or use distinct encoding
-					AudioAttributes audioAttrib = new AudioAttributes.Builder()
-						.setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-						.setUsage(AudioAttributes.USAGE_MEDIA)
-						.build();
-					SoundPool soundPool = new SoundPool.Builder()
-						.setMaxStreams(2)
-						.setAudioAttributes(audioAttrib)
-						.build();
-
-					int sound1 = soundPool.load(sound.getPath(), 1);
-					soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-						public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
-							soundPool.play(sound1, 1.0f, 1.0f, 1, 0, 1.0f);
-						}
-					});*/
-
-					// Android sound code:
-					//int dataLength = DataOperations.getDataFileLength(key + ".pcm", soundData);
-					//int offset = DataOperations.getDataFileOffset(key + ".pcm", soundData);
-					//clientPort.playSound(soundData, offset, dataLength);
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-
-		} catch (RuntimeException var6) {
-			throw GenUtil.makeThrowable(var6, "client.SC(" + "dummy" + ',' + (key != null ? "{...}" : "null") + ')');
-		}
-	}
+        } catch (RuntimeException ignored) {
+        }
+    }
 }
