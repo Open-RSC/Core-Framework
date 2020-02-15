13321c50aaa6270a21b19bd9d143ac26653cfc4e
Marwolf <marwolf@openrsc.com>
2020-02-15 15:07:32 -0500

    Adds OpenPK and Dev Testing launcher capability

--- PC_Launcher/src/com/loader/openrsc/net/Downloader.java
+++ PC_Launcher/src/com/loader/openrsc/net/Downloader.java
@@ -59,7 +59,7 @@ public class Downloader {
 			AppFrame.get().getLaunchcabbage().setEnabled(false);
 			AppFrame.get().getLaunchopenpk().setEnabled(false);
 			//AppFrame.get().getLaunchpreservation().setEnabled(false);
-			//AppFrame.get().getLaunchdev().setEnabled(false);
+			AppFrame.get().getLaunchdev().setEnabled(false);
 			AppFrame.get().setDownloadProgress("Checking for updates...", 100.0f);
 
 			File file = new File(Constants.CONF_DIR);
@@ -164,7 +164,7 @@ public class Downloader {
 			AppFrame.get().getLaunchcabbage().setEnabled(true);
 			AppFrame.get().getLaunchopenpk().setEnabled(true);
 			//AppFrame.get().getLaunchpreservation().setEnabled(true);
-			//AppFrame.get().getLaunchdev().setEnabled(true);
+			AppFrame.get().getLaunchdev().setEnabled(true);
 			AppFrame.get().getSpriteCombo().loadSpritePacks();
 			AppFrame.get().setDownloadProgress("Ready to play!", 100.0f);
 		} catch (Exception e) {
