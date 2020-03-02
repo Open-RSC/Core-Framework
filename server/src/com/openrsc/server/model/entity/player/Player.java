b7c0be6574deb82bad3829cfc2ea0312e8360b93
ScottThomas07 <scottthomas07@gmail.com>
2020-03-02 09:21:16 -0500

    Revert "Fixes an issue where the player menu would remain open after a poison death. Adds a dev command ::poisonme"
    This reverts commit 7a7ef01ebf86f0f3898e5e9a70e2ef94f403db81.

--- server/src/com/openrsc/server/model/entity/player/Player.java
+++ server/src/com/openrsc/server/model/entity/player/Player.java
@@ -1926,14 +1926,6 @@ public final class Player extends Mob {
 			}
 		}
 
-		//Free any NPC that player was talking with (poison)
-		this.resetMenuHandler();
-		if (getInteractingNpc() != null) {
-			getInteractingNpc().setBusy(false);
-			setInteractingNpc(null);
-		}
-
-
 		//getUpdateFlags().setHpUpdate(new HpUpdate(this, 0));
 		getUpdateFlags().reset();
 		//getUpdateFlags().setAppearanceChanged(true);
