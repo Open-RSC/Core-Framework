433d19be55f8b28b3da59683fb59b4eab5242d63
christofosho <csnow@live.ca>
2020-04-07 00:39:09 -0400

    WalkToRadius: Player now walks on top of Npc when pickpocketing, if the player was already in a square directly adjacent to the Npc.

--- server/src/com/openrsc/server/net/rsc/handlers/NpcCommand.java
+++ server/src/com/openrsc/server/net/rsc/handlers/NpcCommand.java
@@ -14,21 +14,23 @@ public final class NpcCommand implements PacketHandler {
 	public void handlePacket(Packet p, Player player) {
 		int pID = p.getID();
 		int serverIndex = p.readShort();
+		if (player == null) return;
 		if (player.isBusy()) {
 			if (player.inCombat()) {
 				player.message("You can't do that whilst you are fighting");
 			}
 			return;
 		}
-		final int click = pID == OpcodeIn.NPC_COMMAND1.getOpcode() ? 0 : 1;
-		player.click = click;
-		final Mob affectedMob = player.getWorld().getNpc(serverIndex);
-		final Npc affectedNpc = (Npc) affectedMob;
-		if (affectedNpc == null || affectedMob == null || player == null)
-			return;
-
+		final boolean click = pID == OpcodeIn.NPC_COMMAND1.getOpcode();
+		player.click = click ? 0 : 1;
+		final Npc affectedNpc = player.getWorld().getNpc(serverIndex);
+		if (affectedNpc == null) return;
+		int radius = 1;
+		if (click && player.withinRange(affectedNpc, 1)) {
+			radius = 0;
+		}
 		player.setFollowing(affectedNpc, 0);
-		player.setWalkToAction(new WalkToMobAction(player, affectedMob, 1) {
+		player.setWalkToAction(new WalkToMobAction(player, affectedNpc, radius) {
 			public void executeInternal() {
 				getPlayer().resetFollowing();
 				getPlayer().resetPath();
@@ -38,13 +40,10 @@ public final class NpcCommand implements PacketHandler {
 				}
 				getPlayer().resetAll();
 				NPCDef def = affectedNpc.getDef();
-				String command = (click == 0 ? def.getCommand1() : def
-					.getCommand2()).toLowerCase();
-				affectedNpc.resetPath();
+				String command = (click ? def.getCommand1() : def.getCommand2()).toLowerCase();
 				getPlayer().getWorld().getServer().getPluginHandler().handlePlugin(getPlayer(), "OpNpc", new Object[]{affectedNpc, command, getPlayer()}, this);
 			}
 		});
-		return;
 	}
 
 }
