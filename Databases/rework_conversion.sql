4e6c6236ed125c3c2c04261f2671fa71146d31b9
christofosho <csnow@live.ca>
2020-04-10 20:24:24 -0400

    Rework DB Conversion Script: Fixes up the script. Tested and working - Needs a test on LIVE data when the new item server code is complete.

--- Databases/rework_conversion.sql
+++ Databases/rework_conversion.sql
@@ -1,23 +1,29 @@
 -- Use this file to place all items to bank and add new inventory layout.
 
+DROP TABLE IF EXISTS `openrsc_itemstatuses`;
 CREATE TABLE IF NOT EXISTS `openrsc_itemstatuses` (
-    `itemID` int(10) UNSIGNED    NOT NULL,
+    `itemID` int(10) UNSIGNED    NOT NULL AUTO_INCREMENT,
     `catalogID`       int(10) UNSIGNED    NOT NULL,
     `amount`   int(10) UNSIGNED NOT NULL DEFAULT 1,
     `noted`  tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
-    `durability`     int(5) UNSIGNED     NOT NULL,
+    `durability`     int(5) UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (`itemID`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-INSERT INTO `openrsc_bank`
-    (`playerID`, `id`, `amount`, `slot`)
-SELECT (`playerID`, `id`, `amount`, 0)
+SELECT '1. openrsc_itemstatuses created.' AS '';
+
+INSERT INTO `openrsc_bank` (`playerID`, `id`, `amount`)
+SELECT `playerID`, `id`, `amount`
 FROM `openrsc_invitems`;
 
-INSERT INTO `openrsc_itemstatuses`
-    (`itemID`, `catalogID`, `amount`, `noted`, `durability`)
-SELECT (row_number(), `id`, `amount`, 0, 0)
+SELECT '2. Items moved from openrsc_invitems to openrsc_bank.' AS '';
+
+INSERT INTO `openrsc_itemstatuses` (`catalogID`, `amount`)
+SELECT `id`, `amount`
+FROM `openrsc_bank`;
+
+SELECT '3. Items updated in openrsc_itemstatuses.' AS '';
 
 DELETE FROM `openrsc_invitems`;
 
@@ -33,15 +39,8 @@ DROP COLUMN `id`;
 ALTER TABLE `openrsc_invitems`
 ADD COLUMN `itemID` int(10) UNSIGNED    NOT NULL;
 
--- CABBAGE
-ALTER TABLE `openrsc_equipped`
-DROP COLUMN `id`;
-
-ALTER TABLE `openrsc_equipped`
-DROP COLUMN `amount`;
-
-ALTER TABLE `openrsc_equipped`
-DROP COLUMN `dbid`;
-
-ALTER TABLE `openrsc_equipped`
-ADD COLUMN `itemID` int(10) UNSIGNED    NOT NULL;
+-- CABBAGE -- UNCOMMENT FOR RUNNING ON CABBAGE!!!!!!!!!!!!
+ALTER TABLE `openrsc_equipped` DROP COLUMN `id`;
+ALTER TABLE `openrsc_equipped` DROP COLUMN `amount`;
+ALTER TABLE `openrsc_equipped` DROP COLUMN `dbid`;
+ALTER TABLE `openrsc_equipped` ADD COLUMN `itemID` int(10) UNSIGNED NOT NULL;
