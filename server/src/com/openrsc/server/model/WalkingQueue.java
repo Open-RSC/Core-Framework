412cf37f2f5b675f09d7d591c03e7519dc1e3240
christofosho <csnow@live.ca>
2020-04-24 23:18:29 -0400

    Npc Positions: Set positions properly on combat start and respawn.

--- server/src/com/openrsc/server/model/WalkingQueue.java
+++ server/src/com/openrsc/server/model/WalkingQueue.java
@@ -203,22 +203,22 @@ public class WalkingQueue {
 		/*
 		 * NPC blocking config controlled
 		 */
-		if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 0) { // No NPC blocks
-			return false;
-		} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 1) { // 2 * combat level + 1 blocks AND aggressive
-			if (npc != null && mob.getCombatLevel() < ((npc.getNPCCombatLevel() * 2) + 1) && npc.getDef().isAggressive()) {
-				return true;
-			}
-		} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 2) { // Any aggressive NPC blocks
-			if (npc != null && npc.getDef().isAggressive()) {
-				return true;
-			}
-		} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 3) { // Any attackable NPC blocks
-			if (npc != null && npc.getDef().isAttackable()) {
-				return true;
-			}
-		} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 4) { // All NPCs block
-			if (npc != null) {
+		if (npc != null) {
+			if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 0) { // No NPC blocks
+				return false;
+			} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 1) { // 2 * combat level + 1 blocks AND aggressive
+				if (mob.getCombatLevel() < ((npc.getNPCCombatLevel() * 2) + 1) && npc.getDef().isAggressive()) {
+					return true;
+				}
+			} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 2) { // Any aggressive NPC blocks
+				if (npc.getDef().isAggressive()) {
+					return true;
+				}
+			} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 3) { // Any attackable NPC blocks
+				if (npc.getDef().isAttackable()) {
+					return true;
+				}
+			} else if (mob.getWorld().getServer().getConfig().NPC_BLOCKING == 4) { // All NPCs block
 				return true;
 			}
 		}
