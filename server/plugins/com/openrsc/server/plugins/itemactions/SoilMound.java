4ffd89f6fabbf8514e02121bbda141a4deb151b1
15rtrujillo <ryantrujillo@comcast.net>
2020-05-03 16:20:53 -0600

    BatchEvent: Removed from SoilMound.java

--- server/plugins/com/openrsc/server/plugins/itemactions/SoilMound.java
+++ server/plugins/com/openrsc/server/plugins/itemactions/SoilMound.java
@@ -1,6 +1,5 @@
 package com.openrsc.server.plugins.itemactions;
 
-import com.openrsc.server.event.custom.BatchEvent;
 import com.openrsc.server.constants.ItemId;
 import com.openrsc.server.model.container.Item;
 import com.openrsc.server.model.entity.GameObject;
@@ -16,27 +15,37 @@ public class SoilMound implements UseLocTrigger {
 		return obj.getID() == 1276 && item.getCatalogId() == ItemId.BUCKET.id();
 	}
 
+	private void batchFill(Player player, Item bucket, int filledId, int repeat) {
+		if (player.getCarriedItems().getInventory().hasInInventory(bucket.getCatalogId())) {
+			thinkbubble(player, bucket);
+			player.message("you fill the bucket with soil");
+			player.getCarriedItems().remove(new Item(bucket.getCatalogId()));
+			give(player, filledId, 1);
+		} else return;
+
+		if (ifinterrupted()) return;
+
+		repeat--;
+		if (repeat > 0) {
+			delay(player.getWorld().getServer().getConfig().GAME_TICK);
+			batchFill(player, bucket, filledId, repeat);
+		}
+	}
+
 	@Override
 	public void onUseLoc(Player player, GameObject obj, final Item item) {
 		final int itemID = item.getCatalogId();
 		final int refilledID = ItemId.SOIL.id();
-		if (item.getCatalogId() != ItemId.BUCKET.id()) {
+		if (itemID != ItemId.BUCKET.id()) {
 			player.message("Nothing interesting happens");
 			return;
 		}
-		player.setBatchEvent(new BatchEvent(player.getWorld(), player, player.getWorld().getServer().getConfig().GAME_TICK, "Fill Bucket with Soil", player.getCarriedItems().getInventory().countId(itemID), true) {
-			@Override
-			public void action() {
-				if (getOwner().getCarriedItems().getInventory().hasInInventory(itemID)) {
-					thinkbubble(getOwner(), item);
-					delay(300);
-					getOwner().message("you fill the bucket with soil");
-					getOwner().getCarriedItems().remove(new Item(itemID));
-					getOwner().getCarriedItems().getInventory().add(new Item(refilledID));
-				} else {
-					interruptBatch();
-				}
-			}
-		});
+
+		int repeat = 1;
+		if (player.getWorld().getServer().getConfig().BATCH_PROGRESSION) {
+			repeat = player.getCarriedItems().getInventory().countId(itemID);
+		}
+
+		batchFill(player, item, refilledID, repeat);
 	}
 }
