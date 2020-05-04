42b67d44a014261af5148b04c1e4229e0a387d0d
15rtrujillo <ryantrujillo@comcast.net>
2020-05-03 18:53:50 -0600

    BatchEvent: Removed from BananaTree.java

--- server/plugins/com/openrsc/server/plugins/misc/BananaTree.java
+++ server/plugins/com/openrsc/server/plugins/misc/BananaTree.java
@@ -1,6 +1,5 @@
 package com.openrsc.server.plugins.misc;
 
-import com.openrsc.server.event.custom.BatchEvent;
 import com.openrsc.server.constants.ItemId;
 import com.openrsc.server.model.entity.GameObject;
 import com.openrsc.server.model.entity.player.Player;
@@ -8,42 +7,56 @@ import com.openrsc.server.plugins.triggers.OpLocTrigger;
 
 import static com.openrsc.server.plugins.Functions.*;
 
-public class BananaTree implements
-	OpLocTrigger {
+public class BananaTree implements OpLocTrigger {
+
+	private final int BANANA_TREE_ID = 183;
+	private final int EMPTY_TREE_ID = 184;
+
+	private void batchBananaPick(Player player, GameObject bananaTree, int repeat) {
+		int tick = player.getWorld().getServer().getConfig().GAME_TICK;
+		int bananaCount = 1;
+		if (player.getCache().hasKey("banana_pick"))
+			bananaCount = player.getCache().getInt("banana_pick") + 1;
+
+		player.getCache().set("banana_pick",bananaCount);
+		give(player, ItemId.BANANA.id(), 1);
+
+		if (bananaCount >= 5) {
+			player.message("you pick the last banana");
+			changeloc(bananaTree, tick * 750, EMPTY_TREE_ID); // 8 minutes respawn time.
+			player.getCache().remove("banana_pick");
+			return;
+		} else {
+			player.message("you pick a banana");
+		}
+
+		if (ifinterrupted()) return;
+
+		repeat--;
+		if (repeat > 0) {
+			delay(tick);
+			batchBananaPick(player, bananaTree, repeat);
+		}
+	}
 
 	@Override
 	public void onOpLoc(Player player, GameObject obj, String command) {
 
-		if (obj.getID() == 183) {
-			player.setBatchEvent(new BatchEvent(player.getWorld(), player, player.getWorld().getServer().getConfig().GAME_TICK, "Pick Banana Tree", player.getCarriedItems().getInventory().getFreeSlots(), false) {
-				@Override
-				public void action () {
-					int bananaCount = 1;
-					if (player.getCache().hasKey("banana_pick"))
-						bananaCount = player.getCache().getInt("banana_pick") + 1;
-
-					player.getCache().set("banana_pick",bananaCount);
-					give(player, ItemId.BANANA.id(), 1);
-
-					if (bananaCount >= 5) {
-						player.message("you pick the last banana");
-						changeloc(obj, 60000 * 8, 184); // 8 minutes respawn time.
-						player.getCache().remove("banana_pick");
-						interruptBatch();
-					} else {
-						player.message("you pick a banana");
-					}
-				}
-			});
+		if (obj.getID() == BANANA_TREE_ID) {
+			int repeat = 1;
+			if (player.getWorld().getServer().getConfig().BATCH_PROGRESSION) {
+				repeat = player.getCarriedItems().getInventory().getFreeSlots();
+			}
+			batchBananaPick(player, obj, repeat);
 		}
 
-		if (obj.getID() == 184) {
+		if (obj.getID() == EMPTY_TREE_ID) {
 			player.message("there are no bananas left on the tree");
 		}
 	}
 
 	@Override
 	public boolean blockOpLoc(Player player, GameObject obj, String command) {
-		return obj.getID() == 183 || obj.getID() == 184;
+		return obj.getID() == BANANA_TREE_ID || obj.getID() == EMPTY_TREE_ID;
 	}
 }
