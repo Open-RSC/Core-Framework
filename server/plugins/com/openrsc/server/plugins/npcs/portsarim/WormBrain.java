0f0b6f865aa8326fe18fb4e553ddb0e7e516f5b1
Christopher Snow <csnow@live.ca>
2020-05-10 08:54:51 -0400

    Menu: Remove from WormBrain.java

--- server/plugins/com/openrsc/server/plugins/npcs/portsarim/WormBrain.java
+++ server/plugins/com/openrsc/server/plugins/npcs/portsarim/WormBrain.java
@@ -11,6 +11,7 @@ import com.openrsc.server.plugins.triggers.OpBoundTrigger;
 import com.openrsc.server.plugins.menu.Menu;
 import com.openrsc.server.plugins.menu.Option;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static com.openrsc.server.plugins.Functions.*;
@@ -30,82 +31,94 @@ public final class WormBrain implements OpBoundTrigger {
 			final Npc n = ifnearvisnpc(player, NpcId.WORMBRAIN.id(), 10);
 			mes(player, "...you knock on the cell door");
 			npcsay(player, n, "Whut you want?");
-			Menu defaultMenu = new Menu();
+
+			ArrayList<String> options = new ArrayList<>();
 			if (player.getQuestStage(Quests.DRAGON_SLAYER) >= 2 && !player.getCarriedItems().hasCatalogID(ItemId.MAP_PIECE_1.id(), Optional.of(false))) {
-				defaultMenu.addOption(new Option("I believe you've got a piece of a map that I need") {
-					@Override
-					public void action() {
-						npcsay(player, n, "So? Why should I be giving it to you? What you do for Wormbrain?");
-						new Menu().addOptions(
-							new Option("I'm not going to do anything for you. Forget it") {
-								public void action() {
-									npcsay(player, n, "Be dat way then");
-								}
-							},
-							new Option("I'll let you live. I could just kill you") {
-								@Override
-								public void action() {
-									npcsay(player, n, "Ha! Me in here and you out dere. You not get map piece");
-								}
-							}, new Option("I suppose I could pay you for the map piece ...") {
-								@Override
-								public void action() {
-									say(player, n, "Say, 500 coins?");
-									npcsay(player, n, "Me not stooped, it worth at least 10,000 coins!");
-									new Menu().addOptions(
-										new Option("You must be joking! Forget it") {
-											public void action() {
-												npcsay(player, n, "Fine, you not get map piece");
-											}
-										}, new Option("Aright then, 10,000 it is") {
-											@Override
-											public void action() {
-												if (ifheld(player, ItemId.COINS.id(), 10000)) {
-													player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 10000));
-													player.message("You buy the map piece from Wormbrain");
-													npcsay(player, n, "Fank you very much! Now me can bribe da guards, hehehe");
-													give(player, ItemId.MAP_PIECE_1.id(), 1);
-												} else {
-													say(player, n, "Oops, I don't have enough on me");
-													npcsay(player, n, "Comes back when you has enough");
-												}
-											}
-										}).showMenu(player);
-								}
-							}, new Option("Where did you get the map piece from?") {
-								@Override
-								public void action() {
-									npcsay(player, n, "We rob house of stupid wizard. She very old, not put up much fight at all. Hahaha!");
-									say(player, n, "Uh ... Hahaha");
-									npcsay(player, n, "Her house full of pictures of a city on island and old pictures of people",
-											"Me not recognise island",
-											"Me find map piece",
-											"Me not know what it is, but it in locked box so me figure it important",
-											"But, by the time me get box open, other goblins gone",
-											"Then me not run fast enough and guards catch me",
-											"But now you want map piece so must be special! What do for me to get it?");
-								}
-							}).showMenu(player);
-					}
-				});
+				options.add("I believe you've got a piece of a map that I need");
 			}
-			defaultMenu.addOption(new Option("What are you in for?") {
-				@Override
-				public void action() {
-					npcsay(player, n, "Me not sure. Me pick some stuff up and take it away");
-					say(player, n, "Well, did the stuff belong to you?");
-					npcsay(player, n, "Umm...no");
-					say(player, n, "Well, that would be why then");
-					npcsay(player, n, "Oh, right");
+			options.add("What are you in for?");
+			options.add("Sorry, thought this was a zoo");
+
+			String[] finalOptions = new String[options.size()];
+			int option = multi(player, n, options.toArray(finalOptions));
+
+			if (option == 0) {
+				if (player.getQuestStage(Quests.DRAGON_SLAYER) >= 2 && !player.getCarriedItems().hasCatalogID(ItemId.MAP_PIECE_1.id(), Optional.of(false))) {
+					dragonSlayerConversation(player, n);
 				}
-			});
-			defaultMenu.addOption(new Option("Sorry, thought this was a zoo") {
-				@Override
-				public void action() {
-					// Nothing
+				else {
+					defaultConversation(player, n);
 				}
-			});
-			defaultMenu.showMenu(player);
+			}
+
+			else if (option == 1) {
+				if (player.getQuestStage(Quests.DRAGON_SLAYER) >= 2 && !player.getCarriedItems().hasCatalogID(ItemId.MAP_PIECE_1.id(), Optional.of(false))) {
+					defaultConversation(player, n);
+				}
+			}
+		}
+	}
+
+	private void defaultConversation(Player player, Npc npc) {
+		npcsay(player, npc, "Me not sure. Me pick some stuff up and take it away");
+		say(player, npc, "Well, did the stuff belong to you?");
+		npcsay(player, npc, "Umm...no");
+		say(player, npc, "Well, that would be why then");
+		npcsay(player, npc, "Oh, right");
+	}
+
+	private void dragonSlayerConversation(Player player, Npc npc) {
+		npcsay(player, npc, "So? Why should I be giving it to you? What you do for Wormbrain?");
+		int option = multi(player, npc,
+			"I'm not going to do anything for you. Forget it",
+			"I'll let you live. I could just kill you",
+			"I suppose I could pay you for the map piece ...",
+			"Where did you get the map piece from?"
+		);
+
+		if (option ==  0) {
+			npcsay(player, npc, "Be dat way then");
+		}
+		else if (option == 1) {
+			npcsay(player, npc, "Ha! Me in here and you out dere. You not get map piece");
+		}
+		else if (option == 2) {
+			say(player, npc, "Say, 500 coins?");
+			npcsay(player, npc, "Me not stooped, it worth at least 10,000 coins!");
+			dragonSlayerPaymentOption(player, npc);
+		}
+		else if (option == 3) {
+			npcsay(player, npc, "We rob house of stupid wizard. She very old, not put up much fight at all. Hahaha!");
+			say(player, npc, "Uh ... Hahaha");
+			npcsay(player, npc, "Her house full of pictures of a city on island and old pictures of people",
+				"Me not recognise island",
+				"Me find map piece",
+				"Me not know what it is, but it in locked box so me figure it important",
+				"But, by the time me get box open, other goblins gone",
+				"Then me not run fast enough and guards catch me",
+				"But now you want map piece so must be special! What do for me to get it?"
+			);
+		}
+	}
+
+	private void dragonSlayerPaymentOption(Player player, Npc npc) {
+		int option = multi(player, npc,
+			"You must be joking! Forget it",
+			"Aright then, 10,000 it is"
+		);
+		if (option == 0) {
+			npcsay(player, npc, "Fine, you not get map piece");
+		}
+		else if (option == 1) {
+			if (ifheld(player, ItemId.COINS.id(), 10000)) {
+				player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 10000));
+				player.message("You buy the map piece from Wormbrain");
+				npcsay(player, npc, "Fank you very much! Now me can bribe da guards, hehehe");
+				give(player, ItemId.MAP_PIECE_1.id(), 1);
+			} else {
+				say(player, npc, "Oops, I don't have enough on me");
+				npcsay(player, npc, "Comes back when you has enough");
+			}
 		}
 	}
 }
