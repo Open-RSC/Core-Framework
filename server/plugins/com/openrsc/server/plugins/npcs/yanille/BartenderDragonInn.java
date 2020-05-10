7bed6271ce6e5e32b22973fba54196b716f6f1d3
15rtrujillo <ryantrujillo@comcast.net>
2020-05-09 22:58:45 -0600

    Removed deprecated Menu from BartenderDragonInn.

--- server/plugins/com/openrsc/server/plugins/npcs/yanille/BartenderDragonInn.java
+++ server/plugins/com/openrsc/server/plugins/npcs/yanille/BartenderDragonInn.java
@@ -1,16 +1,13 @@
 package com.openrsc.server.plugins.npcs.yanille;
 
+import com.openrsc.server.constants.ItemId;
+import com.openrsc.server.constants.NpcId;
 import com.openrsc.server.model.container.Item;
 import com.openrsc.server.model.entity.npc.Npc;
 import com.openrsc.server.model.entity.player.Player;
 import com.openrsc.server.plugins.triggers.TalkNpcTrigger;
-import com.openrsc.server.plugins.menu.Menu;
-import com.openrsc.server.plugins.menu.Option;
 
 import static com.openrsc.server.plugins.Functions.*;
-
-import com.openrsc.server.constants.ItemId;
-import com.openrsc.server.constants.NpcId;
 
 public final class BartenderDragonInn implements
 	TalkNpcTrigger {
@@ -21,40 +18,35 @@ public final class BartenderDragonInn implements
 			npcsay(player, n, "What can I get you?");
 			say(player, n, "What's on the menu?");
 			npcsay(player, n, "Dragon bitter and Greenmans ale");
-			Menu defaultMenu = new Menu();
-			defaultMenu.addOption(new Option("I'll give it a miss I think") {
-				@Override
-				public void action() {
-					npcsay(player, n, "Come back when you're a little thirstier");
+			int option = multi(player, n, "I'll give it a miss I think",
+				"I'll try the dragon bitter",
+				"Can I have some greenmans ale?");
+
+			if (option == 0) {
+				npcsay(player, n, "Come back when you're a little thirstier");
+			}
+
+			else if (option == 1) {
+				npcsay(player, n, "Ok, that'll be two coins");
+				if (ifheld(player, ItemId.COINS.id(), 2)) {
+					player.message("You buy a pint of dragon bitter");
+					give(player, ItemId.DRAGON_BITTER.id(), 1);
+					player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 2));
+				} else {
+					say(player, n, "Oh dear. I don't seem to have enough money");
 				}
-			});
-			defaultMenu.addOption(new Option("I'll try the dragon bitter") {
-				@Override
-				public void action() {
-					npcsay(player, n, "Ok, that'll be two coins");
-					if (ifheld(player, ItemId.COINS.id(), 2)) {
-						player.message("You buy a pint of dragon bitter");
-						give(player, ItemId.DRAGON_BITTER.id(), 1);
-						player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 2));
-					} else {
-						say(player, n, "Oh dear. I don't seem to have enough money");
-					}
+			}
+
+			else if (option == 2) {
+				npcsay(player, n, "Ok, that'll be ten coins");
+				if (ifheld(player, ItemId.COINS.id(), 10)) {
+					player.message("You buy a pint of ale");
+					give(player, ItemId.GREENMANS_ALE.id(), 1);
+					player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 10));
+				} else {
+					say(player, n, "Oh dear. I don't seem to have enough money");
 				}
-			});
-			defaultMenu.addOption(new Option("Can I have some greenmans ale?") {
-				@Override
-				public void action() {
-					npcsay(player, n, "Ok, that'll be ten coins");
-					if (ifheld(player, ItemId.COINS.id(), 10)) {
-						player.message("You buy a pint of ale");
-						give(player, ItemId.GREENMANS_ALE.id(), 1);
-						player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 10));
-					} else {
-						say(player, n, "Oh dear. I don't seem to have enough money");
-					}
-				}
-			});
-			defaultMenu.showMenu(player);
+			}
 		}
 	}
 
