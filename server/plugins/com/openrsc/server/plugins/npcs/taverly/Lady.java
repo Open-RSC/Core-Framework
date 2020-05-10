c4b7d48045a080d7654c4686560028e44843315e
15rtrujillo <ryantrujillo@comcast.net>
2020-05-10 00:53:04 -0600

    Removed deprecated Menu from Lady.

--- server/plugins/com/openrsc/server/plugins/npcs/taverly/Lady.java
+++ server/plugins/com/openrsc/server/plugins/npcs/taverly/Lady.java
@@ -1,14 +1,13 @@
 package com.openrsc.server.plugins.npcs.taverly;
 
 import com.openrsc.server.constants.ItemId;
-import com.openrsc.server.constants.Quests;
 import com.openrsc.server.constants.NpcId;
+import com.openrsc.server.constants.Quests;
 import com.openrsc.server.model.entity.npc.Npc;
 import com.openrsc.server.model.entity.player.Player;
 import com.openrsc.server.plugins.triggers.TalkNpcTrigger;
-import com.openrsc.server.plugins.menu.Menu;
-import com.openrsc.server.plugins.menu.Option;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static com.openrsc.server.plugins.Functions.*;
@@ -18,39 +17,34 @@ public class Lady implements TalkNpcTrigger {
 	@Override
 	public void onTalkNpc(final Player player, final Npc n) {
 		npcsay(player, n, "Good day to you " + (player.isMale() ? "sir" : "madam"));
-		Menu defaultMenu = new Menu();
-		defaultMenu.addOption(new Option("Who are you?") {
-			@Override
-			public void action() {
-				npcsay(player, n, "I am the lady of the lake");
-			}
-		});
-		defaultMenu.addOption(new Option("Good day") {
-			@Override
-			public void action() {
-				// NOTHING HAPPENS
-			}
-		});
+
+		ArrayList<String> options = new ArrayList<>();
+
+		options.add("Who are you?");
+		options.add("Good day");
 		if ((player.getQuestStage(Quests.MERLINS_CRYSTAL) >= 3 || player.getQuestStage(Quests.MERLINS_CRYSTAL) == -1)
 			&& !player.getCarriedItems().hasCatalogID(ItemId.EXCALIBUR.id(), Optional.empty())) {
-			defaultMenu.addOption(new Option("I seek the sword Exalibur") {
-				@Override
-				public void action() {
-					npcsay(player,
-						n,
-						"Aye, I have that artifact in my possession",
-						"Tis very valuable and not an artifact to be given away lightly",
-						"I would want to give it away only to one who is worthy and good");
-					say(player, n, "And how am I meant to prove that");
-					npcsay(player, n, "I will set a test for you",
-						"First I need you to travel to Port Sarim",
-						"Then go to the upstairs room of the jeweller's shop there");
-					say(player, n, "Ok that seems easy enough");
-					player.getCache().store("lady_test", true);
-				}
-			});
+
+			options.add("I seek the sword Exalibur");
 		}
-		defaultMenu.showMenu(player);
+
+		String finalOptions[] = new String[options.size()];
+		int option = multi(player, n, options.toArray(finalOptions));
+		if (option == 0) {
+			npcsay(player, n, "I am the lady of the lake");
+		} else if (option == 2) {
+			npcsay(player,
+				n,
+				"Aye, I have that artifact in my possession",
+				"Tis very valuable and not an artifact to be given away lightly",
+				"I would want to give it away only to one who is worthy and good");
+			say(player, n, "And how am I meant to prove that");
+			npcsay(player, n, "I will set a test for you",
+				"First I need you to travel to Port Sarim",
+				"Then go to the upstairs room of the jeweller's shop there");
+			say(player, n, "Ok that seems easy enough");
+			player.getCache().store("lady_test", true);
+		}
 	}
 
 	@Override
