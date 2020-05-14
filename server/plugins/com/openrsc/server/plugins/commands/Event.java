14e389a6415380d0441d928162fe15b874a12fb0
Christopher Snow <csnow@live.ca>
2020-05-14 10:21:11 -0400

    Event: Add waterfall to locations for TP.

--- server/plugins/com/openrsc/server/plugins/commands/Event.java
+++ server/plugins/com/openrsc/server/plugins/commands/Event.java
@@ -27,14 +27,15 @@ public final class Event implements CommandTrigger {
 
 	private static final String[] towns = {"varrock", "falador", "draynor", "portsarim", "karamja", "alkharid",
 		"lumbridge", "edgeville", "castle", "taverly", "clubhouse", "seers", "barbarian", "rimmington", "catherby",
-		"ardougne", "yanille", "lostcity", "gnome", "shilovillage", "tutorial", "modroom", "entrana"};
+		"ardougne", "yanille", "lostcity", "gnome", "shilovillage", "tutorial", "modroom", "entrana", "waterfall"};
 
 	private static final Point[] townLocations = {Point.location(122, 509), Point.location(304, 542),
 		Point.location(214, 632), Point.location(269, 643), Point.location(370, 685), Point.location(89, 693),
 		Point.location(120, 648), Point.location(217, 449), Point.location(270, 352), Point.location(373, 498),
 		Point.location(653, 491), Point.location(501, 450), Point.location(233, 513), Point.location(325, 663),
 		Point.location(440, 501), Point.location(549, 589), Point.location(583, 747), Point.location(127, 3518),
-		Point.location(703, 527), Point.location(400, 850), Point.location(217, 740), Point.location(75, 1641), Point.location(425,564)};
+		Point.location(703, 527), Point.location(400, 850), Point.location(217, 740), Point.location(75, 1641),
+		Point.location(425,564), Point.location(659, 3302)};
 
 	public boolean blockCommand(Player player, String cmd, String[] args) {
 		return player.isEvent();
@@ -53,7 +54,7 @@ public final class Event implements CommandTrigger {
 			badSyntaxPrefix = player.getWorld().getServer().getConfig().BAD_SYNTAX_PREFIX;
 		}
 
-		if (cmd.equalsIgnoreCase("teleport") || cmd.equalsIgnoreCase("tp") || cmd.equalsIgnoreCase("town") || cmd.equalsIgnoreCase("goto") || cmd.equalsIgnoreCase("tpto") || cmd.equalsIgnoreCase("teleportto")) {
+		if (cmd.equalsIgnoreCase("teleport") || cmd.equalsIgnoreCase("tp") || cmd.equalsIgnoreCase("tele") || cmd.equalsIgnoreCase("town") || cmd.equalsIgnoreCase("goto") || cmd.equalsIgnoreCase("tpto") || cmd.equalsIgnoreCase("teleportto")) {
 			if (args.length < 1) {
 				player.message(badSyntaxPrefix + cmd.toUpperCase() + " [town/player] OR ");
 				player.message(badSyntaxPrefix + cmd.toUpperCase() + " [player] [town/player] OR ");
