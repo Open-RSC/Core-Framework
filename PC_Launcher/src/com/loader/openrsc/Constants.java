dd582e22c90abb1313c95aa2a85a20bad102b47a
Marwolf <marwolf@openrsc.com>
2020-06-06 13:23:14 -0400

    Attempting to solve Launcher not picking up MD5.sum

--- PC_Launcher/src/com/loader/openrsc/Constants.java
+++ PC_Launcher/src/com/loader/openrsc/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
 	public static final String RSCP_WORLD_STATS_URL = "https://runescapeclassic.dev"; // Used for the world statistics
 	public static final String DEV_WORLD_STATS_URL = "https://runescapeclassic.dev"; // Used for the world statistics
 	public static final String Title = "Open RSC Game Launcher";
-	public static final String BASE_URL = "http://game.openrsc.com/"; // Cache and client jar download locations depend on this
+	public static final String BASE_URL = "http://game.openrsc.com/static"; // Cache and client jar download locations depend on this
 	public static final String CONF_DIR = "Cache";
 	public static final String CLIENT_FILENAME = "Open_RSC_Client.jar";
-	public static final String CACHE_URL = BASE_URL + "static/downloads/";
-	public static final Double VERSION_NUMBER = 20200604.072400; //YYYYMMDD.HHMMSS format
+	public static final String CACHE_URL = BASE_URL + "downloads/";
+	public static final Double VERSION_NUMBER = 20200606.132200; //YYYYMMDD.HHMMSS format
 	public static final String VERSION_UPDATE_URL = "https://orsc.dev/open-rsc/Game/raw/master/PC_Launcher/src/com/loader/openrsc/Constants.java";
 	public static final String UPDATE_JAR_URL = "http://game.openrsc.com/static/downloads/OpenRSC.jar";
 	public static final String JAR_FILENAME = "OpenRSC.jar";
