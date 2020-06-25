b51ee851309dbfba81a7fa30f5e538f58924a777
Marwolf <marwolf@openrsc.com>
2020-06-25 11:45:22 +0000

    Deployment script now supports generating a dev client jar

--- Deployment_Scripts/get-updates.sh
+++ Deployment_Scripts/get-updates.sh
@@ -30,7 +30,8 @@ echo "Copy and md5sum compiled client and cache files to the Website downloads f
 
 Choices:
   ${RED}1${NC} - No
-  ${RED}2${NC} - Yes"
+  ${RED}2${NC} - Yes (prod)
+  ${RED}3${NC} - Yes (dev)"
 echo ""
 echo "Type the choice number and press enter."
 echo ""
@@ -56,6 +57,29 @@ elif [ "$compiling" == "2" ]; then
     # Cache copy and file permissions
     sudo chmod 777 -R 'Client_Base/Cache'                                                       # Normal cache related files
     yes | sudo cp -a -rf "Client_Base/Cache/." "../Website/site/public/downloads/"                          # Normal cache related files
+
+    cd '../Website/site/public/downloads/' || exit
+    find -type f \( -not -name "MD5.SUM" \) -exec md5sum '{}' \; >MD5.SUM # Performs md5 hashing of all files in cache and writes to a text file for the launcher to read
+    cd '../../../../Game' || exit
+elif [ "$compiling" == "3" ]; then
+    # PC Client
+	sudo mv Client_Base/*.jar Client_Base/Open_RSC_Client_dev.jar
+    yes | sudo cp -f Client_Base/Open_RSC_Client_dev.jar ../Website/site/public/downloads/
+
+    # Android client
+    #yes | sudo cp -f Android_Client/Open\ RSC\ Android\ Client/*.apk ../Website/site/public/downloads/
+
+    # Launcher
+    #yes | sudo cp -rf PC_Launcher/*.jar ../Website/site/public/downloads/
+
+    # Set file permissions within the Website downloads folder
+    sudo chmod +x ../Website/site/public/downloads/*.jar
+    sudo chmod +x ../Website/site/public/downloads/*.jar
+    sudo chmod -R 777 ../Website/site/public/downloads
+
+    # Cache copy and file permissions
+    #sudo chmod 777 -R 'Client_Base/Cache'                                                       # Normal cache related files
+    #yes | sudo cp -a -rf "Client_Base/Cache/." "../Website/site/public/downloads/"                          # Normal cache related files
 
     cd '../Website/site/public/downloads/' || exit
     find -type f \( -not -name "MD5.SUM" \) -exec md5sum '{}' \; >MD5.SUM # Performs md5 hashing of all files in cache and writes to a text file for the launcher to read
