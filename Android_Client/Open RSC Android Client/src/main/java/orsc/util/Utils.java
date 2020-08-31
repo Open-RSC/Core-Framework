8fb277b21438bb6dd87c2aaed91fcf1ea35fd238
Rrrrry123 <ryantrujillo@comcast.net>
2020-08-30 20:40:11 -0600

    Added the ability for the app to launch the user's web browser so they can use the ::wiki command.

--- Android_Client/Open RSC Android Client/src/main/java/orsc/util/Utils.java
+++ Android_Client/Open RSC Android Client/src/main/java/orsc/util/Utils.java
@@ -1,6 +1,16 @@
 package orsc.util;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
 public class Utils {
 	public static void openWebpage(final String url) {
+		try {
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+			startActivity(browserIntent);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
