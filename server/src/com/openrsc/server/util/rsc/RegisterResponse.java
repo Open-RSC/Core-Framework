057047aefa84cb0958ce5ed0a59e60553472d269
Suli <suli@runescapeclassic.dev>
2020-11-14 14:14:17 -0500

    Add RSC127 compat to register

---
+++ server/src/com/openrsc/server/util/rsc/RegisterResponse.java
@@ -0,0 +1,10 @@
+package com.openrsc.server.util.rsc;
+
+public class RegisterResponse {
+	public static int SERVER_TIMEOUT = -1;
+	public static int REGISTER_UNSUCCESSFUL = 0;
+	public static int REGISTER_SUCCESSFUL = 2;
+	public static int USERNAME_TAKEN = 3;
+	public static int USERNAME_TAKEN_DISALLOWED = 13; //unallowed ones like m0d
+
+}
