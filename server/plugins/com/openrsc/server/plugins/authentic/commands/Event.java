7f13e3ff78749b47928ecd4a12178aa41a48ec49
Hubcapp <walnut.alligator@gmail.com>
2021-01-16 04:03:45 -0600

    add some town names to ::tp

--- server/plugins/com/openrsc/server/plugins/authentic/commands/Event.java
+++ server/plugins/com/openrsc/server/plugins/authentic/commands/Event.java
@@ -29,7 +29,9 @@ public final class Event implements CommandTrigger {
 
 	private static final String[] towns = {"varrock", "falador", "draynor", "portsarim", "karamja", "alkharid",
 		"lumbridge", "edgeville", "castle", "taverly", "clubhouse", "seers", "barbarian", "rimmington", "catherby",
-		"ardougne", "yanille", "lostcity", "gnome", "shilovillage", "tutorial", "modroom", "entrana", "waterfall"};
+		"ardougne", "yanille", "lostcity", "gnome", "shilovillage", "tutorial", "modroom", "entrana", "waterfall",
+		"zanaris", "gertrude", "fishingguild", "taibwowannai", "brimhaven", "shantay", "trawler", "observatory",
+		"crandor", "icemountain", "champion", "hero", "digsite"};
 
 	private static final Point[] townLocations = {Point.location(122, 509), Point.location(304, 542),
 		Point.location(214, 632), Point.location(269, 643), Point.location(370, 685), Point.location(89, 693),
@@ -37,7 +39,10 @@ public final class Event implements CommandTrigger {
 		Point.location(653, 491), Point.location(501, 450), Point.location(233, 513), Point.location(325, 663),
 		Point.location(440, 501), Point.location(549, 589), Point.location(583, 747), Point.location(127, 3518),
 		Point.location(703, 527), Point.location(400, 850), Point.location(217, 740), Point.location(75, 1641),
-		Point.location(425,564), Point.location(659, 3302)};
+		Point.location(425,564), Point.location(659, 3302), Point.location(127,3518), Point.location(160, 515),
+		Point.location(587, 503), Point.location(447, 749), Point.location(446, 694), Point.location(62, 729),
+		Point.location(549, 702), Point.location(713, 697), Point.location(419, 625), Point.location(288, 461),
+		Point.location(151, 556), Point.location(372, 438), Point.location(20, 527)};
 
 	public boolean blockCommand(Player player, String command, String[] args) {
 		return player.isEvent();
