a52e36eda279ec47607624b8127557a64cd7cea1
Marwolf <cleako@gmail.com>
2021-02-04 10:44:01 -0500

    Sets the thread pool to fixed 1 to disable multi threading support

--- server/src/com/openrsc/server/GameEventHandler.java
+++ server/src/com/openrsc/server/GameEventHandler.java
@@ -94,7 +94,8 @@ public class GameEventHandler {
 
 	private void processEvents() {
 		// Update the number of threads in the pool. More servers could have been started so we want to allocate the right amount of threads.
-		final int maxThreads = (Runtime.getRuntime().availableProcessors() * 2) / (Server.serversList.size() > 0 ? Server.serversList.size() : 1);
+		final int maxThreads = 1; // Sets the thread pool to fixed 1
+		//final int maxThreads = (Runtime.getRuntime().availableProcessors() * 2) / (Server.serversList.size() > 0 ? Server.serversList.size() : 1);
 		executor.setMaximumPoolSize(maxThreads);
 		executor.setCorePoolSize(maxThreads / 2);
 
