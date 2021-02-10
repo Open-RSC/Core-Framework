7b061af9aae378139eb23fe9dc700302ded6fdb4
Marwolf <cleako@gmail.com>
2021-02-10 16:29:32 -0500

    Additional gameserver.log clean up

--- server/ant_launcher.sh
+++ server/ant_launcher.sh
@@ -5,5 +5,4 @@ echo ""
 echo "Running the game server. Press CTRL + C to shut it down or"
 echo "CTRL + A + D to detach the screen so this continues in the background."
 echo ""
-touch ../gameserver.log && chmod 777 ../gameserver.log &>/dev/null
 ant runserver
