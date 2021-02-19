8cabe483e9dbc8b138c3c49c172cf941848ff5f6
Marwolf <cleako@gmail.com>
2021-02-18 22:51:11 -0500

    Launcher clean up

--- PC_Launcher/src/com/loader/openrsc/Constants.java
+++
@@ -1,9 +0,0 @@
-/*
- * This file is being preserved to help upgrade existing PC launchers to the new game launcher platform.
- */
-
-package com.loader.openrsc;
-
-public class Constants {
-	public static final Double VERSION_NUMBER = 20210213.135036; //YYYYMMDD.HHMMSS format
-}
