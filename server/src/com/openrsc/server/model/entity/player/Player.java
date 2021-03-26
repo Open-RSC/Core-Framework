a3a737ac15a11e17be97c9b3c404ecde1704d194
Hubcapp <walnut.alligator@gmail.com>
2021-03-26 17:55:36 -0500

    Maybe fix application of double action bug on dropping

--- server/src/com/openrsc/server/model/entity/player/Player.java
+++ server/src/com/openrsc/server/model/entity/player/Player.java
@@ -64,6 +64,7 @@ public final class Player extends Mob {
 	// 1 walked step is +1 activity, 1 5-min warn to move is +25 activity (saved each 30 secs => 2.5 per save)
 	// so everything is multiplied by 2 to avoid decimals
 	private final int KITTEN_ACTIVITY_THRESHOLD = 50;
+	public boolean isDropping = false; // TODO: just fix double action bug lul
 	public int bankSize = getWorld().getServer().getConfig().MEMBER_WORLD ? (getConfig().WANT_CUSTOM_BANKS ? ItemId.maxCustom : 192) : 48; //Maximum bank items allowed
 	private int totalLevel = 0;
 	private Queue<PrivateMessage> privateMessageQueue = new LinkedList<PrivateMessage>();
