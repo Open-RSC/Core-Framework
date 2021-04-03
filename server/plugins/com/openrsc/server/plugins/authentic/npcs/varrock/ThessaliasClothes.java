96ed7f2159a2e699a6dc4a1b08bab49122a38a11
Rrrrry123 <ryantrujillo@comcast.net>
2021-04-03 17:28:17 -0600

    Thessalia will now be able to return lost easter rings. Players can only carry one of each easter ring.

--- server/plugins/com/openrsc/server/plugins/authentic/npcs/varrock/ThessaliasClothes.java
+++ server/plugins/com/openrsc/server/plugins/authentic/npcs/varrock/ThessaliasClothes.java
@@ -52,6 +52,14 @@ public final class ThessaliasClothes extends AbstractShop implements TakeObjTrig
 
 		boolean ears = player.getCarriedItems().hasCatalogID(ItemId.BUNNY_EARS.id()) || player.getBank().countId(ItemId.BUNNY_EARS.id()) > 0;
 		boolean scythe = player.getCarriedItems().hasCatalogID(ItemId.SCYTHE.id()) || player.getBank().countId(ItemId.SCYTHE.id()) > 0;
+		boolean bunnyRing = false;
+		boolean eggRing = false;
+
+		// Check for custom items
+		if (config().WANT_CUSTOM_QUESTS && config().WANT_CUSTOM_SPRITES) {
+			bunnyRing = player.getCarriedItems().hasCatalogID(ItemId.RING_OF_BUNNY.id()) || player.getBank().countId(ItemId.RING_OF_BUNNY.id()) > 0;
+			eggRing = player.getCarriedItems().hasCatalogID(ItemId.RING_OF_EGG.id()) || player.getBank().countId(ItemId.RING_OF_EGG.id()) > 0;
+		}
 
 		ArrayList<String> options = new ArrayList<String>();
 
@@ -63,6 +71,16 @@ public final class ThessaliasClothes extends AbstractShop implements TakeObjTrig
 		String optionEars = "I have lost my bunny ears can I get some more please?";
 		if (player.getCache().hasKey("bunny_ears") && !ears) {
 			options.add(optionEars);
+		}
+
+		String optionBunnyRing = "I have lost my bunny ring can I get another one please?";
+		if (player.getCache().hasKey("ester_rings") && !bunnyRing) {
+			options.add(optionBunnyRing);
+		}
+
+		String optionEggRing = "I have lost my egg ring can I get another one please?";
+		if (player.getCache().hasKey("ester_rings") && !eggRing) {
+			options.add(optionEggRing);
 		}
 
 		String optionShop = "What have you got?";
@@ -89,6 +107,16 @@ public final class ThessaliasClothes extends AbstractShop implements TakeObjTrig
 			say(player, n, "What have you got?");
 			player.setAccessingShop(shop);
 			ActionSender.showShop(player, shop);
+		} else if (options.get(option).equals(optionBunnyRing)) {
+			say(player, n, optionBunnyRing);
+			npcsay(player, n, "Ohh you poor dear, I have another here");
+			player.message("Thessalia gives you a new bunny ring");
+			give(player, ItemId.RING_OF_BUNNY.id(), 1);
+		} else if  (options.get(option).equals(optionEggRing)) {
+			say(player, n, optionEggRing);
+			npcsay(player, n, "Ohh you poor dear, I have another here");
+			player.message("Thessalia gives you a new egg ring");
+			give(player, ItemId.RING_OF_EGG.id(), 1);
 		} else {
 			say(player, n, "No, thank you");
 		}
@@ -120,12 +148,33 @@ public final class ThessaliasClothes extends AbstractShop implements TakeObjTrig
 				player.getCache().put("scythe", LocalDate.now().getYear());
 			}
 		}
+		else if (i.getID() == ItemId.RING_OF_EGG.id()) {
+			if(!player.isAdmin()) {
+				if (player.getCarriedItems().hasCatalogID(ItemId.RING_OF_EGG.id()) || player.getBank().countId(ItemId.RING_OF_EGG.id()) > 0) {
+					player.message("You don't need another egg ring");
+					player.message("You already have one");
+					return;
+				}
+			}
+		}
+		else if (i.getID() == ItemId.RING_OF_BUNNY.id()) {
+			if(!player.isAdmin()) {
+				if (player.getCarriedItems().hasCatalogID(ItemId.RING_OF_BUNNY.id()) || player.getBank().countId(ItemId.RING_OF_BUNNY.id()) > 0) {
+					player.message("You don't need another bunny ring");
+					player.message("You already have one");
+					return;
+				}
+			}
+		}
 
 		player.groundItemTake(i);
 	}
 
 	@Override
 	public boolean blockTakeObj(Player player, GroundItem i) {
-		return i.getID() == ItemId.BUNNY_EARS.id() || i.getID() == ItemId.SCYTHE.id();
+		return i.getID() == ItemId.BUNNY_EARS.id()
+			|| i.getID() == ItemId.SCYTHE.id()
+			|| i.getID() == ItemId.RING_OF_BUNNY.id()
+			|| i.getID() == ItemId.RING_OF_EGG.id();
 	}
 }
