88afe4eb79ba977c1cd018ca79bc23af999d5ca7
Marwolf <marwolf@rsc.vet>
2021-11-27 09:09:55 -0500

    Removes automated Android client build code which does not work

--- Deployment_Scripts/get-updates.sh
+++ Deployment_Scripts/get-updates.sh
@@ -26,22 +26,22 @@ ant -f PC_Launcher/build.xml compile
 # Export the Android SDK path
 #sudo snap install androidsdk
 #sudo snap install gradle --classic
-export ANDROID_SDK_ROOT=/home/wolf/AndroidSDK
-export PATH=$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$PATH
+#export ANDROID_SDK_ROOT=/home/wolf/AndroidSDK
+#export PATH=$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$PATH
 
-ANDROID_COMPILE_SDK="31"
-ANDROID_BUILD_TOOLS="31.0.0"
+#ANDROID_COMPILE_SDK="31"
+#ANDROID_BUILD_TOOLS="31.0.0"
 
-echo y | androidsdk --version &>/dev/null
-echo y | androidsdk --update &>/dev/null
-yes | androidsdk --sdk_root=$ANDROID_SDK_ROOT --licenses &>/dev/null
-echo y | androidsdk --sdk_root=$ANDROID_SDK_ROOT "platforms;android-$ANDROID_COMPILE_SDK" &>/dev/null
-echo y | androidsdk --sdk_root=$ANDROID_SDK_ROOT "platform-tools" &>/dev/null
-echo y | androidsdk --sdk_root=$ANDROID_SDK_ROOT "build-tools;$ANDROID_BUILD_TOOLS" &>/dev/null
-export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools/
+#echo y | androidsdk --version &>/dev/null
+#echo y | androidsdk --update &>/dev/null
+#yes | androidsdk --sdk_root=$ANDROID_SDK_ROOT --licenses &>/dev/null
+#echo y | androidsdk --sdk_root=$ANDROID_SDK_ROOT "platforms;android-$ANDROID_COMPILE_SDK" &>/dev/null
+#echo y | androidsdk --sdk_root=$ANDROID_SDK_ROOT "platform-tools" &>/dev/null
+#echo y | androidsdk --sdk_root=$ANDROID_SDK_ROOT "build-tools;$ANDROID_BUILD_TOOLS" &>/dev/null
+#export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools/
 
 # Gradle compile Android client
-gradle -b Android_Client/Open\ RSC\ Android\ Client/build.gradle assembleDebug
+#gradle -b Android_Client/Open\ RSC\ Android\ Client/build.gradle assembleDebug
 
 # Launcher
 echo ""
@@ -50,7 +50,6 @@ echo "Copy and md5sum compiled client and cache files to the Website downloads f
 Choices:
   ${RED}1${NC} - No
   ${RED}2${NC} - Yes (prod)
-  ${RED}3${NC} - Yes (dev)"
 echo ""
 echo "Type the choice number and press enter."
 echo ""
@@ -63,7 +62,7 @@ elif [ "$compiling" == "2" ]; then
     yes | sudo cp -f Client_Base/*.jar ../Website/portal/public/downloads/
 
     # Android client
-    yes | sudo find . -type f -path '*Android_Client*/*Open RSC Android Client*/*' -name \*.apk | xargs -I {} sudo cp "{}" ../Website/portal/public/downloads/
+    #yes | sudo find . -type f -path '*Android_Client*/*Open RSC Android Client*/*' -name \*.apk | xargs -I {} sudo cp "{}" ../Website/portal/public/downloads/
     ##yes | sudo cp -f Android_Client/Open\ RSC\ Android\ Client/*.apk ../Website/portal/public/downloads/
 
     # Launcher
@@ -72,37 +71,12 @@ elif [ "$compiling" == "2" ]; then
     # Set file permissions within the Website downloads folder
     sudo chmod +x ../Website/portal/public/downloads/*.jar
     sudo chmod +x ../Website/portal/public/downloads/*.jar
-    sudo chmod +x ../Website/portal/public/downloads/*.apk
+    #sudo chmod +x ../Website/portal/public/downloads/*.apk
     sudo chmod -R 777 ../Website/portal/public/downloads
 
     # Cache copy and file permissions
     sudo chmod 777 -R 'Client_Base/Cache'                                                       # Normal cache related files
     yes | sudo cp -a -rf "Client_Base/Cache/." "../Website/portal/public/downloads/"                          # Normal cache related files
-
-    cd '../Website/portal/public/downloads/' || exit
-    find -type f \( -not -name "MD5.SUM" \) -exec md5sum '{}' \; >MD5.SUM # Performs md5 hashing of all files in cache and writes to a text file for the launcher to read
-    cd '../../../../Game' || exit
-elif [ "$compiling" == "3" ]; then
-    # PC Client
-        sudo mv Client_Base/*.jar Client_Base/Open_RSC_Client_dev.jar
-    yes | sudo cp -f Client_Base/Open_RSC_Client_dev.jar ../Website/portal/public/downloads/
-
-    # Android client
-    yes | sudo find . -type f -path '*Android_Client*/*Open RSC Android Client*/*' -name \*.apk | xargs -I {} sudo cp "{}" ../Website/portal/public/downloads/
-    ##yes | sudo cp -f Android_Client/Open\ RSC\ Android\ Client/*.apk ../Website/portal/public/downloads/
-
-    # Launcher
-    #yes | sudo cp -rf PC_Launcher/*.jar ../Website/portal/public/downloads/
-
-    # Set file permissions within the Website downloads folder
-    sudo chmod +x ../Website/portal/public/downloads/*.jar
-    sudo chmod +x ../Website/portal/public/downloads/*.jar
-    sudo chmod +x ../Website/portal/public/downloads/*.apk
-    sudo chmod -R 777 ../Website/portal/public/downloads
-
-    # Cache copy and file permissions
-    #sudo chmod 777 -R 'Client_Base/Cache'                                                       # Normal cache related files
-    #yes | sudo cp -a -rf "Client_Base/Cache/." "../Website/portal/public/downloads/"                          # Normal cache related files
 
     cd '../Website/portal/public/downloads/' || exit
     find -type f \( -not -name "MD5.SUM" \) -exec md5sum '{}' \; >MD5.SUM # Performs md5 hashing of all files in cache and writes to a text file for the launcher to read
