6eaad9613b9f9261e82668dfcd4ddf89f4fda231
Chomp <9366621-chomp9@users.noreply.gitlab.com>
2022-05-17 14:20:41 -0600

    cleanup Prayers

--- server/src/com/openrsc/server/model/entity/player/Prayers.java
+++ server/src/com/openrsc/server/model/entity/player/Prayers.java
@@ -3,36 +3,35 @@ package com.openrsc.server.model.entity.player;
 import com.openrsc.server.net.rsc.ActionSender;
 
 public class Prayers {
-
-	public static int THICK_SKIN = 0, BURST_OF_STRENGTH = 1, CLARITY_OF_THOUGHT = 2, ROCK_SKIN = 3,
+	public static final int THICK_SKIN = 0, BURST_OF_STRENGTH = 1, CLARITY_OF_THOUGHT = 2, ROCK_SKIN = 3,
 		SUPERHUMAN_STRENGTH = 4, IMPROVED_REFLEXES = 5, RAPID_RESTORE = 6, RAPID_HEAL = 7, PROTECT_ITEMS = 8,
 		STEEL_SKIN = 9, ULTIMATE_STRENGTH = 10, INCREDIBLE_REFLEXES = 11, PARALYZE_MONSTER = 12,
 		PROTECT_FROM_MISSILES = 13;
 
-	private Player player;
-	private boolean[] activatedPrayers = new boolean[14];
+	private final boolean[] activatedPrayers = new boolean[14];
+	private final Player player;
 
-	public Prayers(Player player) {
+	public Prayers(final Player player) {
 		this.player = player;
 	}
 
-	public boolean isPrayerActivated(int pID) {
-		return activatedPrayers[pID];
+	public boolean isPrayerActivated(final int prayerID) {
+		return activatedPrayers[prayerID];
 	}
 
 	public boolean[] getActivePrayers() {
-	    return activatedPrayers;
-    }
+		return activatedPrayers;
+	}
 
-	public void setPrayer(int pID, boolean b) {
-		activatedPrayers[pID] = b;
+	public void setPrayer(final int prayerID, final boolean activated) {
+		activatedPrayers[prayerID] = activated;
 		ActionSender.sendPrayers(player, activatedPrayers);
 	}
 
 	public void resetPrayers() {
-		for (int x = 0; x < activatedPrayers.length; x++) {
-			if (activatedPrayers[x]) {
-				activatedPrayers[x] = false;
+		for (int i = 0; i < activatedPrayers.length; i++) {
+			if (activatedPrayers[i]) {
+				activatedPrayers[i] = false;
 			}
 		}
 		ActionSender.sendPrayers(player, activatedPrayers);
