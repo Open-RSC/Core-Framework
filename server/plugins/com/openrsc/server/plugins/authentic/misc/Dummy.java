9ca6f9387763bf754ec59cdd1d99a88b48da6c47
Hubcapp <walnut.alligator@gmail.com>
2022-06-03 00:18:19 -0500

    audit of dummies

--- server/plugins/com/openrsc/server/plugins/authentic/misc/Dummy.java
+++ server/plugins/com/openrsc/server/plugins/authentic/misc/Dummy.java
@@ -1,9 +1,12 @@
 package com.openrsc.server.plugins.authentic.misc;
 
+import com.google.inject.spi.Message;
 import com.openrsc.server.constants.Skill;
 import com.openrsc.server.model.entity.GameObject;
 import com.openrsc.server.model.entity.player.Player;
+import com.openrsc.server.net.rsc.ActionSender;
 import com.openrsc.server.plugins.triggers.OpLocTrigger;
+import com.openrsc.server.util.rsc.MessageType;
 
 import static com.openrsc.server.plugins.Functions.delay;
 import static com.openrsc.server.plugins.Functions.mes;
@@ -17,29 +20,40 @@ public class Dummy implements OpLocTrigger {
 
 	@Override
 	public void onOpLoc(Player player, GameObject obj, String command) {
-		mes("You swing at the dummy");
-		delay(5);
 		if (obj.getID() == 49) { // Dummy
-			if (player.getSkills().getLevel(Skill.ATTACK.id()) > 7) {
-				player.message("There is only so much you can learn from hitting a dummy");
-			} else {
-				player.message("You hit the dummy");
+			mes("You swing at the dummy");
+			delay(5);
+			player.playerServerMessage(MessageType.QUEST, "You hit the dummy");
+			ActionSender.sendSound(player, "combat1");
+			if (player.getSkills().getLevel(Skill.ATTACK.id()) <= 7) {
+				// this situation not found in replays
 				player.incExp(Skill.ATTACK.id(), 20, true);
+			} else {
+				player.playerServerMessage(MessageType.QUEST, "There is nothing more you can learn from hitting a dummy");
 			}
 		} else if (obj.getID() == 562) { // fight Dummy
+			boolean grantXP = false;
 			if (player.getCache().hasKey("combat_dummy")) {
 				if (player.getCache().getInt("combat_dummy") < 10) {
 					player.getCache().set("combat_dummy", player.getCache().getInt("combat_dummy") + 1);
-				} else {
-					player.message("There is nothing more you can learn from hitting this dummy");
-					return;
+					grantXP = true;
 				}
-			} else
+			} else {
 				player.getCache().set("combat_dummy", 1);
+				grantXP = true;
+			}
 
-			// TODO: Proper message for this prop.
-			player.message("You hit the dummy");
-			player.incExp(Skill.ATTACK.id(), 200, true);
+			if (grantXP) {
+				player.incExp(Skill.ATTACK.id(), 200, true);
+			}
+			mes("You swing at the dummy");
+			delay(5);
+			player.playerServerMessage(MessageType.QUEST, "You hit the dummy");
+			ActionSender.sendSound(player, "combat1");
+			if (!grantXP) {
+				// this situation not found in replays
+				player.playerServerMessage(MessageType.QUEST, "There is nothing more you can learn from hitting this dummy");
+			}
 		}
 	}
 }
