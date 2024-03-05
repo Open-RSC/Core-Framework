cc8f1bc4819dd22fde60c53c9ccf2dcd71a2cbba
Ryan <6275015-Rrrrry123@users.noreply.gitlab.com>
2024-03-05 06:08:42 +0000

    Fixed Yohnus for UIM

--- server/plugins/com/openrsc/server/plugins/authentic/npcs/shilo/Yohnus.java
+++ server/plugins/com/openrsc/server/plugins/authentic/npcs/shilo/Yohnus.java
@@ -1,6 +1,7 @@
 package com.openrsc.server.plugins.authentic.npcs.shilo;
 
 import com.openrsc.server.constants.ItemId;
+import com.openrsc.server.constants.IronmanMode;
 import com.openrsc.server.constants.NpcId;
 import com.openrsc.server.model.container.Item;
 import com.openrsc.server.model.entity.GameObject;
@@ -24,22 +25,38 @@ public class Yohnus implements TalkNpcTrigger, OpBoundTrigger {
 		}
 	}
 
-	public boolean takeFromBank(Player player) {
+	public boolean fastYohnus(Player player) {
+		if (player.getIronMan() == IronmanMode.Ultimate.id()) {
+			if (ifheld(player, ItemId.COINS.id(), 20)) {
+				player.getCarriedItems().remove(new Item(ItemId.COINS.id(), 20));
+				return true;
+			}
+		}
+
 		if (player.getBank().countId(ItemId.COINS.id()) >= 20) {
 			player.getBank().remove(ItemId.COINS.id(), 20, false);
 			return true;
 		}
+
 		return false;
 	}
 
 	private void yohnusChat(Player player, Npc n) {
 		boolean fastPayConfig = player.getWorld().getServer().getConfig().FASTER_YOHNUS && !player.getQolOptOut();
+		boolean isUltimate = player.getIronMan() == IronmanMode.Ultimate.id();
+
 		ArrayList<String> options = new ArrayList<>();
 		// Authentic options
 		options.add("Use Furnace - 20 Gold");
 		options.add("No thanks!");
 		// Custom option
-		if (fastPayConfig) options.add("Take from Bank Until Logout");
+		if (fastPayConfig) {
+			if (isUltimate) {
+				options.add("Take from Inventory Until Logout");
+			} else {
+				options.add("Take from Bank Until Logout");
+			}
+		}
 
 		final String finalOptions[] = new String[options.size()];
 		int menu = multi(player, n, false, //do not send over
@@ -58,15 +75,15 @@ public class Yohnus implements TalkNpcTrigger, OpBoundTrigger {
 			say(player, n, "No thanks!");
 			npcsay(player, n, "Very well Bwana, have a nice day.");
 		} else if (fastPayConfig && menu == 2) {
-			say(player, n, "Sure, you can just take it from my bank");
-			if (takeFromBank(player)) {
+			say(player, n, "Sure, you can just take it from my " + (isUltimate ? "inventory" : "bank"));
+			if (fastYohnus(player)) {
 				npcsay(player, n, "Thanks Bwana!",
 					"Enjoy the facilities!");
 				player.teleport(400, 844);
 				player.setAttribute("fast_yohnus", true);
 			} else {
 				npcsay(player, n, "Sorry Bwana",
-					"You don't have enough coins in your bank");
+					"You don't have enough coins in your " + (isUltimate ? "inventory" : "bank"));
 				player.setAttribute("fast_yohnus", false);
 			}
 		}
@@ -93,12 +110,14 @@ public class Yohnus implements TalkNpcTrigger, OpBoundTrigger {
 			// Custom fast pay feature
 			boolean fastPayConfig = player.getWorld().getServer().getConfig().FASTER_YOHNUS && !player.getQolOptOut();
 			boolean fastPay = (boolean)player.getAttribute("fast_yohnus", false);
+
 			if (fastPayConfig && fastPay) {
-				if (takeFromBank(player)) {
-					player.message("Yohnus takes 20 coins from your bank and shows you inside");
+				boolean isUltimate = player.getIronMan() == IronmanMode.Ultimate.id();
+				if (fastYohnus(player)) {
+					player.message("Yohnus takes 20 coins from your " + (isUltimate ? "inventory" : "bank") + " and shows you inside");
 					player.teleport(400, 844);
 				} else {
-					player.message("You don't have enough coins in your bank");
+					player.message("You don't have enough coins in your " + (isUltimate ? "inventory" : "bank"));
 					player.setAttribute("fast_yohnus", false);
 				}
 				return;
