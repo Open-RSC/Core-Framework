027b9b8ceea85adea5161deea00746b1ffcd4efe
Stormy <stormy@mailfence.com>
2024-03-26 17:37:45 +0000

    Stop non-damaging spell projectiles from stealing NPC aggression

--- server/src/com/openrsc/server/event/rsc/impl/projectile/CustomProjectileEvent.java
+++ server/src/com/openrsc/server/event/rsc/impl/projectile/CustomProjectileEvent.java
@@ -3,6 +3,7 @@ package com.openrsc.server.event.rsc.impl.projectile;
 import com.openrsc.server.model.entity.Mob;
 import com.openrsc.server.model.entity.npc.Npc;
 import com.openrsc.server.model.entity.player.Player;
+import com.openrsc.server.model.states.CombatState;
 import com.openrsc.server.model.world.World;
 
 public abstract class CustomProjectileEvent extends ProjectileEvent {
@@ -19,8 +20,12 @@ public abstract class CustomProjectileEvent extends ProjectileEvent {
 	public void action() {
 		if (!canceled) {
 			doSpell();
-			if (opponent.isNpc() && caster.isPlayer() && this.shouldChase) {
-				((Npc) opponent).setChasing((Player) caster);
+			if (opponent.isNpc() && caster.isPlayer()) {
+				Npc npc = (Npc) opponent;
+				Player player = (Player) caster;
+				if (!npc.isChasing() && !npc.inCombat() && npc.getCombatState() != CombatState.RUNNING && this.shouldChase) {
+					npc.setChasing(player);
+				}
 			}
 		}
 	}
