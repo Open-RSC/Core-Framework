1ee8496aea154b13a452a107a89116f87483caa1
Ryan <6275015-Rrrrry123@users.noreply.gitlab.com>
2026-01-17 20:17:38 +0000

    Small Bank Logic Fix

--- server/src/com/openrsc/server/model/container/Bank.java
+++ server/src/com/openrsc/server/model/container/Bank.java
@@ -598,6 +598,10 @@ public class Bank {
 
 			// Add the item to the inventory (or fail and place it back into the bank).
 			if (!player.getCarriedItems().getInventory().add(item, updateClient)) {
+				// If the client doesn't support the item, DON'T ADD IT BACK because it dropped on the ground.
+				if (player.getClientLimitations().maxItemId < item.getCatalogId()) {
+					return;
+				}
 				add(item);
 			}
 		} else {
@@ -611,6 +615,10 @@ public class Bank {
 
 				// Add the item to the inventory (or fail and place it back into the bank).
 				if (!player.getCarriedItems().getInventory().add(item, updateClient)) {
+					// If the client doesn't support the item, DON'T ADD IT BACK because it dropped on the ground.
+					if (player.getClientLimitations().maxItemId < item.getCatalogId()) {
+						break;
+					}
 					add(item);
 					break;
 				}
