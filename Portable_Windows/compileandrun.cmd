8111ab10bbc97cefe06f5da95faa8310a0488f42
ipkpjersi <ipkpjersi@gmail.com>
2026-05-19 12:06:32 -0400

    Fixed Windows scripts race condition

--- Portable_Windows/compileandrun.cmd
+++ Portable_Windows/compileandrun.cmd
@@ -7,7 +7,26 @@ SET "PATH=zulu8.50.0.51-ca-jdk8.0.275-win_x64\bin\;%PATH%"
 
 cls
 echo: Starting up the server and then launching the client. Close this window when you are finished playing.
-call START /min "" %antpath%ant -f ../server\build.xml compile-and-run && PING localhost -n 20 >NUL && call START "" %antpath%ant -f ../Client_Base\build.xml compile-and-run
+echo:
+
+REM Read the port from port.txt
+SET /P serverport=<..\Client_Base\Cache\port.txt
+echo Server port: %serverport%
+echo:
+echo Compiling and starting server...
+call START /min "" %antpath%ant -f ../server\build.xml compile-and-run
+
+:wait_for_server
+echo Waiting for server to be ready on port %serverport%...
+PING localhost -n 6 >NUL
+netstat -an | find ":%serverport%" | find "LISTENING" >NUL
+if errorlevel 1 (
+    echo Server not ready yet, waiting...
+    goto wait_for_server
+)
+
+echo Server is ready! Starting client...
+call START "" %antpath%ant -f ../Client_Base\build.xml compile-and-run
 
 pause
 taskkill /F /IM Java*
--- Portable_Windows/run.cmd
+++ Portable_Windows/run.cmd
@@ -7,7 +7,26 @@ SET "PATH=zulu8.50.0.51-ca-jdk8.0.275-win_x64\bin\;%PATH%"
 
 cls
 echo: Starting up the server and then launching the client.
-call START /min "" %antpath%ant -f ../server\build.xml runserver && PING localhost -n 11 >NUL && call START "" %antpath%ant -f ../Client_Base\build.xml runclient
+echo:
+
+REM Read the port from port.txt
+SET /P serverport=<..\Client_Base\Cache\port.txt
+echo Server port: %serverport%
+echo:
+echo Starting server...
+call START /min "" %antpath%ant -f ../server\build.xml runserver
+
+:wait_for_server
+echo Waiting for server to be ready on port %serverport%...
+PING localhost -n 6 >NUL
+netstat -an | find ":%serverport%" | find "LISTENING" >NUL
+if errorlevel 1 (
+    echo Server not ready yet, waiting...
+    goto wait_for_server
+)
+
+echo Server is ready! Starting client...
+call START "" %antpath%ant -f ../Client_Base\build.xml runclient
 
 pause
 taskkill /F /IM Java*
